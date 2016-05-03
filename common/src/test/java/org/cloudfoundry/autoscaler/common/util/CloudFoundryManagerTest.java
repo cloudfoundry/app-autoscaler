bcd1becc57414264edffc9168ed31506aa9c9dad
Bo Yang <byang@us.ibm.com>
2016-05-02 17:05:29 -0700

    Better error handling when retriving service info and test cases for cloud foundry manager [#118497757]
    - error handling in cloudfoundry manager
    - mock up of CC API for getting environment variables
    - More test cases for cloudfoundy manager
    - remove useless config in service broker

--- common/src/test/java/org/cloudfoundry/autoscaler/common/util/CloudFoundryManagerTest.java
+++ common/src/test/java/org/cloudfoundry/autoscaler/common/util/CloudFoundryManagerTest.java
@@ -6,14 +6,20 @@ import com.sun.jersey.test.framework.JerseyTest;
 
 import static org.junit.Assert.fail;
 
+import org.cloudfoundry.autoscaler.common.AppNotFoundException;
 import org.cloudfoundry.autoscaler.common.ClientIDLoginFailedException;
+import org.cloudfoundry.autoscaler.common.CloudException;
 import org.cloudfoundry.autoscaler.common.Constants;
+import org.cloudfoundry.autoscaler.common.ServiceNotFoundException;
 import org.cloudfoundry.autoscaler.common.util.CloudFoundryManager;
+import static org.cloudfoundry.autoscaler.common.rest.mock.cc.CloudControllerRestApi.TEST_APP_ID;
+import static org.cloudfoundry.autoscaler.common.rest.mock.cc.CloudControllerRestApi.TEST_SEVICE_NAME;
+
 import org.cloudfoundry.autoscaler.common.util.ConfigManager;
 
-public class CloudFoundryLoginTest extends JerseyTest {
+public class CloudFoundryManagerTest extends JerseyTest {
 
-	public CloudFoundryLoginTest() throws Exception {
+	public CloudFoundryManagerTest() throws Exception {
 		super("org.cloudfoundry.autoscaler.common.rest.mock.cc");
 	}
 
@@ -47,5 +53,37 @@ public class CloudFoundryLoginTest extends JerseyTest {
 				ConfigManager.get(Constants.CFURL));
 		manager.login();
 	}
+	
+	@Test
+	public void testGetServiceInfo(){
+		try{
+			CloudFoundryManager manager = CloudFoundryManager.getInstance();
+			manager.getServiceInfo(TEST_APP_ID, TEST_SEVICE_NAME);
+		}
+		catch(Exception e){
+			fail("Get exception when gettting service info");
+		}	
+	}
+
+	@Test(expected = CloudException.class)
+	public void testGetServiceInfoWithNullToken() throws Exception {
+		CloudFoundryManager manager = new CloudFoundryManager();
+		manager.getServiceInfo(TEST_APP_ID, TEST_SEVICE_NAME);
+	}
+	
+	
+	@Test(expected = AppNotFoundException.class)
+	public void testGetServiceInfoWithNonExistAppID() throws Exception {
+		CloudFoundryManager manager = CloudFoundryManager.getInstance();
+		manager.getServiceInfo("non_exist_ID", TEST_SEVICE_NAME);
+	}
+
+	@Test(expected = ServiceNotFoundException.class)
+	public void testGetServiceInfoWithNonExistService() throws Exception {
+		CloudFoundryManager manager = CloudFoundryManager.getInstance();
+		manager.getServiceInfo(TEST_APP_ID, "non_exsit_service_name");
+	}
+
+	
 
 }
