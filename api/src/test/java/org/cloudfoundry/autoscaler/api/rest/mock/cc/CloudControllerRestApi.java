65f8908224694e7aa32be1545793eaf3c0acada5
Michael Fraenkel <fraenkel@us.ibm.com>
2016-06-13 14:27:13 -0400

    Remove couchdb based code

--- api/src/test/java/org/cloudfoundry/autoscaler/api/rest/mock/cc/CloudControllerRestApi.java
+++
@@ -1,7 +0,0 @@
-package org.cloudfoundry.autoscaler.api.rest.mock.cc;
-
-import javax.ws.rs.Path;
-
-@Path("/")
-public class CloudControllerRestApi extends org.cloudfoundry.autoscaler.common.rest.mock.cc.CloudControllerRestApi {
-}
--- server/src/main/java/org/cloudfoundry/autoscaler/schedule/ScalingScheduledServiceFactory.java
+++
@@ -1,8 +0,0 @@
-package org.cloudfoundry.autoscaler.schedule;
-
-public class ScalingScheduledServiceFactory {
-
-	public static ScheduledService getScheduledService() {
-		return DefaultScheduledService.getInstance();
-	}
-}
