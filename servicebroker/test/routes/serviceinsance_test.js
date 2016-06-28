d09de6b608a0aee89d381adc32c72c2e2cc5aa8d
Liu Ying <cdlliuy@cn.ibm.com>
2016-06-28 14:32:38 +0800

    Deprovision service
    [#119505891]

--- servicebroker/test/routes/serviceinsance_test.js
+++ servicebroker/test/routes/serviceinsance_test.js
@@ -11,19 +11,19 @@ var settings = JSON.parse(
 var auth = new Buffer(settings.username + ":" + settings.password).toString('base64');
 
 
-describe('provisionService RESTful API', function() {
-  var server, serviceId, orgId, spaceId, orgIdAgain, spaceIdAgain;
-  serviceId = uuid.v4();
+describe('service instance RESTful API', function() {
+  var server, serviceInstanceId, orgId, spaceId, orgIdAgain, spaceIdAgain;
+  serviceInstanceId = uuid.v4();
   orgId = uuid.v4();
   spaceId = uuid.v4();
   orgIdAgain = uuid.v4();
   spaceIdAgain = uuid.v4();
 
+
   before(function() {
     delete require.cache[require.resolve('../../lib/index.js')];
     server = require(path.join(__dirname, '../../lib/index.js'));
     serviceInstance.sequelize.sync();
-    serviceInstance.truncate({ cascade: true });
   });
 
   after(function(done) {
@@ -34,43 +34,14 @@ describe('provisionService RESTful API', function() {
     serviceInstance.truncate({ cascade: true });
   });
 
-
-  it("creates a new instance", function(done) {
-    supertest(server)
-      .put("/v2/service_instances/" + serviceId)
-      .set("Authorization", "Basic " + auth)
-      .send({ "organization_guid": orgId, "space_guid": spaceId })
-      .expect(201)
-      .expect('Content-Type', /json/)
-      .expect({
-        dashboard_url: ''
-      }, done);
-  });
-
-  context('when an instance already exists', function(done) {
-
-    beforeEach(function(done) {
-      serviceId = uuid.v4();
-      orgId = uuid.v4();
-      spaceId = uuid.v4();
-      supertest(server)
-        .put("/v2/service_instances/" + serviceId)
-        .set("Authorization", "Basic " + auth)
-        .send({ "organization_guid": orgId, "space_guid": spaceId })
-        .expect(201)
-        .expect('Content-Type', /json/)
-        .expect({
-          dashboard_url: ''
-        }, done);
-    });
-
-    context('when orgId and spaceId are identical', function(done) {
-      it('returns a 200', function(done) {
+  context('Provision service ', function() {
+    context('when there is no record', function() {
+      it("creates a new instance with 201", function(done) {
         supertest(server)
-          .put("/v2/service_instances/" + serviceId)
+          .put("/v2/service_instances/" + serviceInstanceId)
           .set("Authorization", "Basic " + auth)
           .send({ "organization_guid": orgId, "space_guid": spaceId })
-          .expect(200)
+          .expect(201)
           .expect('Content-Type', /json/)
           .expect({
             dashboard_url: ''
@@ -78,20 +49,86 @@ describe('provisionService RESTful API', function() {
       });
     });
 
-    context('when orgId and spaceId are conflict to previous record', function(done) {
+    context('when an instance already exists', function() {
 
-      it('returns a 409', function(done) {
+      beforeEach(function(done) {
         supertest(server)
-          .put("/v2/service_instances/" + serviceId)
+          .put("/v2/service_instances/" + serviceInstanceId)
           .set("Authorization", "Basic " + auth)
-          .send({ "organization_guid": orgIdAgain, "space_guid": spaceIdAgain })
-          .expect(409, done);
+          .send({ "organization_guid": orgId, "space_guid": spaceId })
+          .expect(201)
+          .expect('Content-Type', /json/)
+          .expect({
+            dashboard_url: ''
+          }, done);
       });
+
+      context('when orgId and spaceId are identical', function() {
+        it('returns a 200', function(done) {
+          supertest(server)
+            .put("/v2/service_instances/" + serviceInstanceId)
+            .set("Authorization", "Basic " + auth)
+            .send({ "organization_guid": orgId, "space_guid": spaceId })
+            .expect(200)
+            .expect('Content-Type', /json/)
+            .expect({
+              dashboard_url: ''
+            }, done);
+        });
+      });
+
+      context('when orgId and spaceId are conflict to previous record', function() {
+        it('returns a 409', function(done) {
+          supertest(server)
+            .put("/v2/service_instances/" + serviceInstanceId)
+            .set("Authorization", "Basic " + auth)
+            .send({ "organization_guid": orgIdAgain, "space_guid": spaceIdAgain })
+            .expect(409, done);
+        });
+      });
+    });
+
+  });
+
+
+  context('Deprovision service ', function() {
+
+  context('when there is no record', function() {
+    it("delete an nonexist instance with 410", function(done) {
+      supertest(server)
+        .delete("/v2/service_instances/" + "nonexistid")
+        .set("Authorization", "Basic " + auth)
+        .expect(410)
+        .expect('Content-Type', /json/)
+        .expect({}, done);
+    });
+  });
+
+  context('when an instance already exists', function() {
+
+     beforeEach(function(done) {
+        supertest(server)
+          .put("/v2/service_instances/" + serviceInstanceId)
+          .set("Authorization", "Basic " + auth)
+          .send({ "organization_guid": orgId, "space_guid": spaceId })
+          .expect(201)
+          .expect('Content-Type', /json/)
+          .expect({
+            dashboard_url: ''
+          }, done);
+      });
+
+    it("delete an instance with 200", function(done) {
+      supertest(server)
+        .delete("/v2/service_instances/" + serviceInstanceId)
+        .set("Authorization", "Basic " + auth)
+        .expect(200)
+        .expect('Content-Type', /json/)
+        .expect({}, done);
     });
   });
 
 
-
-
+  });
 
 });
