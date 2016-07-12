535081646b71fd8093139039f6893f5af73f51da
I044137 <pradyut.sarma@sap.com>
2016-07-12 16:54:11 +0530

    Validate Autoscaler Policy

--- api/test/unit/models/index.test.js
+++ api/test/unit/models/index.test.js
@@ -2,11 +2,15 @@
 
 var expect = require("chai").expect;
 var logger = require('../../../lib/log/logger');
+var _ = require('underscore');
 
 describe('Model Initialization', function () {
   it('returns the Policy model', function () {
     var models = require('../../../lib/models')();
     logger.log('Policy model loaded successfully');
     expect(models.policy_json).to.be.ok;
+    expect(_.has(models, 'policy_json')).to.be.equal(true);
+    expect(_.has(models, 'sequelize')).to.be.equal(true);
+    expect(_.keys(models).length).to.be.equal(2);
   });
 });
