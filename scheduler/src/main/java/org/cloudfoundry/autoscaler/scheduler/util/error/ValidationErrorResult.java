d1745851781599f8051573f9cb0c718286b4cc0e
ghaih <harpreetg@fast.au.fujitsu.com>
2016-07-15 12:19:37 +1000

    Added the missing annotation.

--- scheduler/src/main/java/org/cloudfoundry/autoscaler/scheduler/util/error/ValidationErrorResult.java
+++ scheduler/src/main/java/org/cloudfoundry/autoscaler/scheduler/util/error/ValidationErrorResult.java
@@ -7,24 +7,26 @@ import org.quartz.SchedulerException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.stereotype.Component;
-
-
+import org.springframework.web.context.annotation.RequestScope;
 
 /**
- * 
+ * Request scoped object for tracking results of validation (business rules validation mostly).
  *
  */
+
 @Component
 @Configurable
+@RequestScope
 public class ValidationErrorResult {
-	
-	@Autowired MessageBundleResourceHelper messageBundleResourceHelper;
+
+	@Autowired
+	MessageBundleResourceHelper messageBundleResourceHelper;
 	boolean initialised;
-	private List<ValidationError> errorList; // NOTE: lLeave error list null until, have actual errors
+	private List<ValidationError> errorList; // NOTE:Leave error list null until, have actual errors
 
 	public ValidationErrorResult() {
 	}
-	
+
 	/**
 	 * Initialise an instance with an empty errors list and no association with a specific object type
 	 * @param objectName
@@ -33,30 +35,29 @@ public class ValidationErrorResult {
 		errorList = null;
 		initialised = true;
 	}
-	
+
 	private void checkInitialised() {
 		if (!initialised) {
 			throw new InternalCodingError("ValidationResult not initialised - call  initEmpty");
 		}
 	}
-	
+
 	public void addFieldError(Object object, String messageCode, Object... arguments) {
-		
+
 		internalAddError(new ValidationError(object, arguments, messageCode));
 	}
-	
-	public void addErrorForDatabaseValidationException(DatabaseValidationException dve, String messageCode, Object... arguments) {
+
+	public void addErrorForDatabaseValidationException(DatabaseValidationException dve, String messageCode,
+			Object... arguments) {
 		addFieldError(dve, messageCode, arguments);
-		
-		
+
 	}
-	
+
 	public void addErrorForQuartzSchedulerException(SchedulerException se, String messageCode, Object... arguments) {
 		addFieldError(se, messageCode, arguments);
-	
-	
+
 	}
-	
+
 	private void internalAddError(ValidationError error) {
 		checkInitialised();
 		if (errorList == null) {
@@ -64,7 +65,7 @@ public class ValidationErrorResult {
 		}
 		errorList.add(error);
 	}
-	
+
 	/**
 	 * A list of error messages corresponding to the errors contained in this instance
 	 * @return a List<String> containing the error messages.
@@ -74,23 +75,23 @@ public class ValidationErrorResult {
 		if (errorList == null || errorList.size() == 0) {
 			return new ArrayList<String>();
 		}
-		
+
 		List<String> errorMessages = new ArrayList<String>(errorList.size());
-		
+
 		for (ValidationError error : errorList) {
-			
+
 			String resourceKey = error.getErrorMessageCode();
 			Object[] messageArguments = error.getErrorMessageArguments();
-			
+
 			// Lookup error with arguments
 			String errorMessage = messageBundleResourceHelper.lookupMessage(resourceKey, messageArguments);
-		
+
 			errorMessages.add(errorMessage);
 		}
-		
+
 		return errorMessages;
 	}
-	
+
 	/**
 	 * @return - true if this instance contains any errors
 	 */
@@ -98,5 +99,5 @@ public class ValidationErrorResult {
 		checkInitialised();
 		return errorList != null && errorList.size() > 0;
 	}
-	
+
 }
