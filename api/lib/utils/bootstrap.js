931a7f8b683c7e6b698b35d8073d7ccfbcd3f32c
Pradyut Sarma <pradyutksarma@gmail.com>
2016-09-20 12:20:30 +0530

    Code Refactoring and update apiserver README

--- api/lib/utils/bootstrap.js
+++ api/lib/utils/bootstrap.js
@@ -1,18 +1,32 @@
-exports.boot = function boot(callback) {
-  if(!process.env.DB_URI) {
-    callback({ 'message':'DB_URI not found' });
-  }
-  else if(!process.env.SCHEDULER_URI) {
-    callback({ 'message': 'SCHEDULER_URI not found' });
-  }
-  else{
-    require('../../lib/models')(function(error) {
-      if (error) {
-        callback(error)
+var async = require ('async');
+
+exports.boot = function boot(cb) {
+  async.series([
+    function(callback) {
+      if (!process.env.DB_URI) {
+        callback({ 'message':'DB_URI not found' });		
+      } else {
+	    callback(null);
+	  }
+    },
+    function(callback) {
+	  if (!process.env.SCHEDULER_URI) {
+        callback({ 'message':'SCHEDULER_URI not found' });		
+      } else {
+        callback (null);
       }
-      else{
-        callback(null);
-      }
-    });
-  }
+    },
+    function(callback) {
+      require('../../lib/models')(function(error) {
+        if (error) {
+          callback(error)
+        } else{
+          callback(null);
+        }
+      });
+    },
+  ],
+  function(err, results) {
+    cb (err);
+  });
 }
