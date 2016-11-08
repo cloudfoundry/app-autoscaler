6db35b664c55dd0bccfae52ae741d6e0d4932d24
Bo Yang <bo.yang.9527@gmail.com>
2016-11-08 06:05:14 -0800

    store raw app instances metrics to database (#118)
    Store raw app instances metrics to database
    
    - get raw instance metrics from loggregator
    - store raw instance metrics to metrics database
    - add index for appinstancemetrics table and revise test case
    
    Note:
    1. using the raw instance metrics for aggregation will be done in story #131839361
    2. will remove previous metrics table and related code after story #131839361 is done
    
    [#131839215]
    
    - create index for table appinstancemetrics
    - update test case of instancemetrics_sqldb
    - update submodule github.com/onsi/gomega to include gstruct matchers

---
+++ db/sqldb/instancemetrics_sqldb.go
@@ -0,0 +1,118 @@
+package sqldb
+
+import (
+	"database/sql"
+	"time"
+
+	"code.cloudfoundry.org/lager"
+	_ "github.com/lib/pq"
+
+	"autoscaler/db"
+	"autoscaler/models"
+)
+
+type InstanceMetricsSQLDB struct {
+	logger lager.Logger
+	url    string
+	sqldb  *sql.DB
+}
+
+func NewInstanceMetricsSQLDB(url string, logger lager.Logger) (*InstanceMetricsSQLDB, error) {
+	sqldb, err := sql.Open(db.PostgresDriverName, url)
+	if err != nil {
+		logger.Error("failed-open-instancemetrics-db", err, lager.Data{"url": url})
+		return nil, err
+	}
+
+	err = sqldb.Ping()
+	if err != nil {
+		sqldb.Close()
+		logger.Error("failed-ping-instancemetrics-db", err, lager.Data{"url": url})
+		return nil, err
+	}
+
+	return &InstanceMetricsSQLDB{
+		sqldb:  sqldb,
+		logger: logger,
+		url:    url,
+	}, nil
+}
+
+func (idb *InstanceMetricsSQLDB) Close() error {
+	err := idb.sqldb.Close()
+	if err != nil {
+		idb.logger.Error("failed-close-instancemetrics-db", err, lager.Data{"url": idb.url})
+		return err
+	}
+	return nil
+}
+
+func (idb *InstanceMetricsSQLDB) SaveMetric(metric *models.AppInstanceMetric) error {
+	query := "INSERT INTO appinstancemetrics(appid, instanceindex, collectedat, name, unit, value, timestamp) values($1, $2, $3, $4, $5, $6, $7)"
+	_, err := idb.sqldb.Exec(query, metric.AppId, metric.InstanceIndex, metric.CollectedAt, metric.Name, metric.Unit, metric.Value, metric.Timestamp)
+
+	if err != nil {
+		idb.logger.Error("failed-insert-instancemetric-into-appinstancemetrics-table", err, lager.Data{"query": query, "metric": metric})
+	}
+	return err
+}
+
+func (idb *InstanceMetricsSQLDB) RetrieveInstanceMetrics(appid string, name string, start int64, end int64) ([]*models.AppInstanceMetric, error) {
+	query := "SELECT instanceindex, collectedat, unit, value, timestamp FROM appinstancemetrics WHERE " +
+		" appid = $1 " +
+		" AND name = $2 " +
+		" AND timestamp >= $3" +
+		" AND timestamp <= $4" +
+		" ORDER BY timestamp, instanceindex"
+
+	if end < 0 {
+		end = time.Now().UnixNano()
+	}
+
+	rows, err := idb.sqldb.Query(query, appid, name, start, end)
+	if err != nil {
+		idb.logger.Error("failed-retrieve-instancemetrics-from-appinstancemetrics-table", err,
+			lager.Data{"query": query, "appid": appid, "metricName": name, "start": start, "end": end})
+		return nil, err
+	}
+	defer rows.Close()
+
+	mtrcs := []*models.AppInstanceMetric{}
+	var index uint32
+	var collectedAt, timestamp int64
+	var unit, value string
+
+	for rows.Next() {
+		if err = rows.Scan(&index, &collectedAt, &unit, &value, &timestamp); err != nil {
+			idb.logger.Error("failed-scan-instancemetric-from-search-result", err)
+			return nil, err
+		}
+
+		length := len(mtrcs)
+		if (length > 0) && (timestamp == mtrcs[length-1].Timestamp) && (index == mtrcs[length-1].InstanceIndex) {
+			continue
+		}
+
+		metric := models.AppInstanceMetric{
+			AppId:         appid,
+			InstanceIndex: index,
+			CollectedAt:   collectedAt,
+			Name:          name,
+			Unit:          unit,
+			Value:         value,
+			Timestamp:     timestamp,
+		}
+		mtrcs = append(mtrcs, &metric)
+	}
+	return mtrcs, nil
+}
+
+func (idb *InstanceMetricsSQLDB) PruneInstanceMetrics(before int64) error {
+	query := "DELETE FROM appinstancemetrics WHERE timestamp <= $1"
+	_, err := idb.sqldb.Exec(query, before)
+	if err != nil {
+		idb.logger.Error("failed-prune-instancemetric-from-appinstancemetrics-table", err, lager.Data{"query": query, "before": before})
+	}
+
+	return err
+}
--- models/metrics.go
+++ models/metrics.go
@@ -31,6 +31,35 @@ type InstanceMetric struct {
 	Value     string `json:"value"`
 }
 
+type AppInstanceMetric struct {
+	AppId         string `json:"app_id"`
+	InstanceIndex uint32 `json:"instance_index"`
+	CollectedAt   int64  `json:"collected_at"`
+	Name          string `json:"name"`
+	Unit          string `json:"unit"`
+	Value         string `json:"value"`
+	Timestamp     int64  `json:"timestamp"`
+}
+
+func GetInstanceMemoryMetricFromContainerEnvelopes(collectAt int64, appId string, containerEnvelopes []*events.Envelope) []*AppInstanceMetric {
+	metrics := []*AppInstanceMetric{}
+	for _, e := range containerEnvelopes {
+		cm := e.ContainerMetric
+		if *cm.ApplicationId == appId {
+			metrics = append(metrics, &AppInstanceMetric{
+				AppId:         appId,
+				InstanceIndex: uint32(cm.GetInstanceIndex()),
+				CollectedAt:   collectAt,
+				Name:          MetricNameMemory,
+				Unit:          UnitBytes,
+				Value:         fmt.Sprintf("%d", cm.GetMemoryBytes()),
+				Timestamp:     e.GetTimestamp(),
+			})
+		}
+	}
+	return metrics
+}
+
 func GetMemoryMetricFromContainerMetrics(appId string, containerMetrics []*events.Envelope) *Metric {
 	insts := []InstanceMetric{}
 	for _, e := range containerMetrics {
