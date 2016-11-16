6e67ac5f176bec0014dd677feb019e1bdf8c2953
qiyangbj@cn.ibm.com <qiyangbj@cn.ibm.com>
2016-11-16 17:30:33 +0800

    use promise-pattern to call commit and rollback

--- servicebroker/lib/routes/binding.js
+++ servicebroker/lib/routes/binding.js
@@ -10,6 +10,23 @@ module.exports = function(app, settings) {
 
   var apiServerUtil = require(path.join(__dirname, '../util/apiServerUtil.js'))(settings.apiServerUri);
   var messageUtil = require(path.join(__dirname, '../util/messageUtil.js'))();
+
+  function commitTransaction(transaction, response, statusCode, responseBody) {
+    transaction.commit().then(function(res) {
+      response.status(statusCode).json(responseBody || {});
+    }).catch(function(err) {
+      response.status(500).json(responseBody || {});
+    });
+  }
+
+  function rollbackTransaction(transaction, response, statusCode, responseBody) {
+    transaction.rollback().then(function(res) {
+      response.status(statusCode).json(responseBody || {});
+    }).catch(function(err) {
+      response.status(500).json(responseBody || {});
+    });
+  }
+
   app.put('/v2/service_instances/:instance_id/service_bindings/:binding_id', function(req, res) {
     var serviceInstanceId = req.params.instance_id;
     var bindingId = req.params.binding_id;
@@ -48,31 +65,27 @@ module.exports = function(app, settings) {
                   var statusCode = response.statusCode;
                   logger.info("Api Server response", { status_code: statusCode, response: response.body });
                   if (statusCode === 200 || statusCode === 201) {
-                    t.commit();
-                    res.status(statusCode).json({});
+                    commitTransaction(t, res, statusCode, {});
                   } else {
-                    t.rollback();
                     if (statusCode === 400 || statusCode === 500) {
-                      res.status(statusCode).json({});
+                      rollbackTransaction(t, res, statusCode, {});
                     } else {
-                      res.status(500).json({});
+                      rollbackTransaction(t, res, 500, {});
                     }
                   }
                 } else {
                   logger.error("Bind failed: attach policy error", { error: error });
-                  res.status(500).json({});
-                  t.rollback();
+                  rollbackTransaction(t, res, 500, {});
                 }
               });
             }).catch(function(error1) { //catch findorcreate
               logger.error("Bind failed: create error", { error: error1 });
-              t.rollback();
               if (error1 instanceof models.sequelize.UniqueConstraintError) {
-                res.status(409).json({});
+                rollbackTransaction(t, res, 409, {});
               } else if (error1 instanceof models.sequelize.ForeignKeyConstraintError) {
-                res.status(404).json({ "description": messageUtil.getMessage("SERVICEINSTANCE_NOT_EXIST", { "serviceInstanceId": serviceInstanceId }) });
+                rollbackTransaction(t, res, 404, { "description": messageUtil.getMessage("SERVICEINSTANCE_NOT_EXIST", { "serviceInstanceId": serviceInstanceId }) });
               } else {
-                res.status(500).json({});
+                rollbackTransaction(t, res, 500, {});
               }
             });
           } else if (length > 1) { // an app has been bound to more than one service instance, this error should not exist
@@ -91,8 +104,7 @@ module.exports = function(app, settings) {
         });
       }).catch(function(error2) { //catch transaction
         logger.error("Bind failed: transaction error", { error: error2 });
-        t.rollback();
-        res.status(500).json({});
+        rollbackTransaction(t, res, 500, {});
       });
     }
 
@@ -123,45 +135,38 @@ module.exports = function(app, settings) {
                   var statusCode = response.statusCode;
                   logger.info("Api Server response", { status_code: statusCode, response: response.body });
                   if (statusCode === 200) {
-                    t.commit();
-                    res.status(statusCode).json({});
+                    commitTransaction(t, res, statusCode, {});
                   } else if (statusCode === 404) {
-                    t.commit();
-                    res.status(200).json({});
+                    commitTransaction(t, res, 200, {});
                   } else { //for 400,500 and other status, return 500
                     logger.error("Unbind failed: detach policy failed", { status_code: statusCode });
-                    t.rollback();
-                    res.status(500).json({});
+                    rollbackTransaction(t, res, 500, {});
                   }
                 } else {
-                  t.rollback();
+                  
                   logger.error("Bind failed: detach policy error", { error: error });
-                  res.status(500).json({});
+                  rollbackTransaction(t, res, 500, {});
                 }
 
               });
             } else {
-              t.commit();
-              res.status(410).json({});
+              commitTransaction(t, res, 410, {});
             }
           }).catch(function(error1) {
             logger.error("Unbind failed: destroy error", { error: error1 });
-            t.rollback();
-            res.status(500).json({});
+            rollbackTransaction(t, res, 500, {});
+
           });
         } else {
-          t.commit();
-          res.status(410).json({});
+          commitTransaction(t, res, 410, {});
         }
       }).catch(function(error3) {
         logger.error("Unbind failed: find binding failed", { error: error3 });
-        t.rollback();
-        res.status(500).json({});
+        rollbackTransaction(t, res, 500, {});
       });
     }).catch(function(error2) {
       logger.error("Unbind failed: transaction error", { error: error2 });
-      t.rollback();
-      res.status(500).json({});
+      rollbackTransaction(t, res, 500, {});
     });
   });
 }
