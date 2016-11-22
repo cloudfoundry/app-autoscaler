b0fe64434cbcb5c655c607ea681fa5fcaa663996
Bo Yang <byang@us.ibm.com>
2016-11-22 12:45:55 -0800

    add TLS support for metrics collector and scaling engine endpoint [#121340317]
    - add SSL config for metrics collector and scaling engine
    - add SSL support for http servers
    - test https in main

--- src/autoscaler/metricscollector/cmd/metricscollector/metricscollector_test.go
+++ src/autoscaler/metricscollector/cmd/metricscollector/metricscollector_test.go
@@ -104,8 +104,7 @@ var _ = Describe("MetricsCollector", func() {
 			})
 
 			It("returns with a 200", func() {
-				rsp, err := http.Get(fmt.Sprintf("http://127.0.0.1:%d/v1/apps/an-app-id/metrics/memory", mcPort))
-
+				rsp, err := httpClient.Get(fmt.Sprintf("https://localhost:%d/v1/apps/an-app-id/metrics/memory", mcPort))
 				Expect(err).NotTo(HaveOccurred())
 				Expect(rsp.StatusCode).To(Equal(http.StatusOK))
 				rsp.Body.Close()
@@ -119,8 +118,7 @@ var _ = Describe("MetricsCollector", func() {
 				eLock.Unlock()
 			})
 			It("refreshes the token and returns with a 200", func() {
-				rsp, err := http.Get(fmt.Sprintf("http://127.0.0.1:%d/v1/apps/an-app-id/metrics/memory", mcPort))
-
+				rsp, err := httpClient.Get(fmt.Sprintf("https://localhost:%d/v1/apps/an-app-id/metrics/memory", mcPort))
 				Expect(err).NotTo(HaveOccurred())
 				Expect(rsp.StatusCode).To(Equal(http.StatusOK))
 				rsp.Body.Close()
@@ -131,8 +129,7 @@ var _ = Describe("MetricsCollector", func() {
 
 	Describe("when a request for memory metrics history comes", func() {
 		It("returns with a 200", func() {
-			rsp, err := http.Get(fmt.Sprintf("http://127.0.0.1:%d/v1/apps/an-app-id/metric_histories/memory", mcPort))
-
+			rsp, err := httpClient.Get(fmt.Sprintf("https://localhost:%d/v1/apps/an-app-id/metric_histories/memory", mcPort))
 			Expect(err).NotTo(HaveOccurred())
 			Expect(rsp.StatusCode).To(Equal(http.StatusOK))
 			rsp.Body.Close()
