a720e00b0b4e37456de069136ed1950ba6a39427
qiyangbj@cn.ibm.com <qiyangbj@cn.ibm.com>
2016-12-02 15:23:48 +0800

    refactor event-generator components
    1 policyPoller starts in main
    2 aggregator and appEvaluationManager can getPolices

--- src/autoscaler/eventgenerator/generator/appEvaluationManager.go
+++ src/autoscaler/eventgenerator/generator/appEvaluationManager.go
@@ -21,9 +21,11 @@ type AppEvaluationManager struct {
 	triggerChan      chan []*model.Trigger
 	triggers         map[string][]*model.Trigger
 	evaluatorArray   []*Evaluator
+	getPolicies      model.GetPolicies
 }
 
-func NewAppEvaluationManager(evaluateInterval time.Duration, logger lager.Logger, cclock clock.Clock, triggerChan chan []*model.Trigger, evaluatorCount int, database db.AppMetricDB, scalingEngineUrl string) *AppEvaluationManager {
+func NewAppEvaluationManager(evaluateInterval time.Duration, logger lager.Logger, cclock clock.Clock, triggerChan chan []*model.Trigger,
+	evaluatorCount int, database db.AppMetricDB, scalingEngineUrl string, getPolicies model.GetPolicies) *AppEvaluationManager {
 	manager := &AppEvaluationManager{
 		evaluateInterval: evaluateInterval,
 		logger:           logger.Session("AppEvaluationManager"),
@@ -32,6 +34,7 @@ func NewAppEvaluationManager(evaluateInterval time.Duration, logger lager.Logger
 		triggerChan:      triggerChan,
 		triggers:         map[string][]*model.Trigger{},
 		evaluatorArray:   []*Evaluator{},
+		getPolicies:      getPolicies,
 	}
 	client := cfhttp.NewClient()
 	client.Transport.(*http.Transport).MaxIdleConnsPerHost = evaluatorCount
@@ -40,6 +43,32 @@ func NewAppEvaluationManager(evaluateInterval time.Duration, logger lager.Logger
 		manager.evaluatorArray = append(manager.evaluatorArray, evaluator)
 	}
 	return manager
+}
+func (a *AppEvaluationManager) getTriggers(policyMap map[string]*model.Policy) map[string][]*model.Trigger {
+	if policyMap == nil {
+		return nil
+	}
+	var triggerArrayMap map[string][]*model.Trigger = make(map[string][]*model.Trigger)
+	for appId, policy := range policyMap {
+		for _, rule := range policy.TriggerRecord.ScalingRules {
+			triggerKey := appId + "#" + rule.MetricType
+			triggerArray, exist := triggerArrayMap[triggerKey]
+			if !exist {
+				triggerArray = []*model.Trigger{}
+			}
+			triggerArray = append(triggerArray, &model.Trigger{
+				AppId:            appId,
+				MetricType:       rule.MetricType,
+				BreachDuration:   rule.BreachDuration,
+				CoolDownDuration: rule.CoolDownDuration,
+				Threshold:        rule.Threshold,
+				Operator:         rule.Operator,
+				Adjustment:       rule.Adjustment,
+			})
+			triggerArrayMap[triggerKey] = triggerArray
+		}
+	}
+	return triggerArrayMap
 }
 func (a *AppEvaluationManager) Start() {
 	for _, evaluator := range a.evaluatorArray {
@@ -62,23 +91,13 @@ func (a *AppEvaluationManager) doEvaluate() {
 		case <-a.doneChan:
 			return
 		case <-ticker.C():
-			a.evaluate()
+			a.lock.Lock()
+			a.triggers = a.getTriggers(a.getPolicies())
+			a.lock.Unlock()
+			for _, triggerArray := range a.triggers {
+				a.triggerChan <- triggerArray
+			}
 		}
 	}
 	a.logger.Info("started")
-}
-
-func (a *AppEvaluationManager) SetTriggers(triggers map[string][]*model.Trigger) {
-	a.lock.Lock()
-	a.triggers = triggers
-	a.lock.Unlock()
-}
-
-func (a *AppEvaluationManager) evaluate() {
-	a.lock.Lock()
-	triggers := a.triggers
-	a.lock.Unlock()
-	for _, triggerArray := range triggers {
-		a.triggerChan <- triggerArray
-	}
 }
--- src/autoscaler/eventgenerator/generator/appEvaluationManager_test.go
+++ src/autoscaler/eventgenerator/generator/appEvaluationManager_test.go
@@ -4,21 +4,21 @@ import (
 	"autoscaler/eventgenerator/aggregator/fakes"
 	. "autoscaler/eventgenerator/generator"
 	. "autoscaler/eventgenerator/model"
-	"net/http"
-	"regexp"
-	"time"
-
 	"code.cloudfoundry.org/clock/fakeclock"
 	"code.cloudfoundry.org/lager"
 	"code.cloudfoundry.org/lager/lagertest"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"github.com/onsi/gomega/ghttp"
+	"net/http"
+	"regexp"
+	"time"
 )
 
 var _ = Describe("AppEvaluationManager", func() {
 
 	var (
+		getPolicies          GetPolicies
 		logger               lager.Logger
 		fclock               *fakeclock.FakeClock
 		manager              *AppEvaluationManager
@@ -28,47 +28,124 @@ var _ = Describe("AppEvaluationManager", func() {
 		triggerArrayChan     chan []*Trigger
 		testAppId            string = "testAppId"
 		testAppId2           string = "testAppId2"
-		testMetricType       string = "MemoeryUsage"
+		testAppId3           string = "testAppId3"
+		testAppId4           string = "testAppId4"
+		testMetricType       string = "MemoryUsage"
 		fakeScalingEngine    *ghttp.Server
-		regPath                                    = regexp.MustCompile(`^/v1/apps/.*/scale$`)
-		testMap              map[string][]*Trigger = map[string][]*Trigger{
-			testAppId + "#" + testMetricType: []*Trigger{&Trigger{
-				AppId:            testAppId,
-				MetricType:       testMetricType,
-				BreachDuration:   300,
-				CoolDownDuration: 300,
-				Threshold:        80,
-				Operator:         ">",
-				Adjustment:       "1",
-			}, &Trigger{
-				AppId:            testAppId,
-				MetricType:       testMetricType,
-				BreachDuration:   300,
-				CoolDownDuration: 300,
-				Threshold:        30,
-				Operator:         "<",
-				Adjustment:       "-1",
-			}},
+		regPath                                 = regexp.MustCompile(`^/v1/apps/.*/scale$`)
+		policyMap            map[string]*Policy = map[string]*Policy{
+			testAppId: &Policy{
+				AppId: testAppId,
+				TriggerRecord: &TriggerRecord{
+					InstanceMaxCount: 5,
+					InstanceMinCount: 1,
+					ScalingRules: []*ScalingRule{
+						&ScalingRule{
+							MetricType:       "MemoryUsage",
+							StatWindow:       300,
+							BreachDuration:   300,
+							CoolDownDuration: 300,
+							Threshold:        30,
+							Operator:         "<",
+							Adjustment:       "-1",
+						},
+					},
+				},
+			},
+			testAppId2: &Policy{
+				AppId: testAppId2,
+				TriggerRecord: &TriggerRecord{
+					InstanceMaxCount: 5,
+					InstanceMinCount: 1,
+					ScalingRules: []*ScalingRule{
+						&ScalingRule{
+							MetricType:       "MemoryUsage",
+							StatWindow:       300,
+							BreachDuration:   300,
+							CoolDownDuration: 300,
+							Threshold:        30,
+							Operator:         "<",
+							Adjustment:       "-1",
+						},
+					},
+				},
+			},
+			testAppId3: &Policy{
+				AppId: testAppId3,
+				TriggerRecord: &TriggerRecord{
+					InstanceMaxCount: 5,
+					InstanceMinCount: 1,
+					ScalingRules: []*ScalingRule{
+						&ScalingRule{
+							MetricType:       "MemoryUsage",
+							StatWindow:       300,
+							BreachDuration:   300,
+							CoolDownDuration: 300,
+							Threshold:        30,
+							Operator:         "<",
+							Adjustment:       "-1",
+						},
+					},
+				},
+			},
+			testAppId4: &Policy{
+				AppId: testAppId4,
+				TriggerRecord: &TriggerRecord{
+					InstanceMaxCount: 5,
+					InstanceMinCount: 1,
+					ScalingRules: []*ScalingRule{
+						&ScalingRule{
+							MetricType:       "MemoryUsage",
+							StatWindow:       300,
+							BreachDuration:   300,
+							CoolDownDuration: 300,
+							Threshold:        30,
+							Operator:         "<",
+							Adjustment:       "-1",
+						},
+					},
+				},
+			},
 		}
-
-		testMap2 map[string][]*Trigger = map[string][]*Trigger{
-			testAppId2 + "#" + testMetricType: []*Trigger{&Trigger{
-				AppId:            testAppId2,
-				MetricType:       testMetricType,
-				BreachDuration:   300,
-				CoolDownDuration: 300,
-				Threshold:        80,
-				Operator:         ">",
-				Adjustment:       "1",
-			}, &Trigger{
-				AppId:            testAppId2,
-				MetricType:       testMetricType,
-				BreachDuration:   300,
-				CoolDownDuration: 300,
-				Threshold:        30,
-				Operator:         "<",
-				Adjustment:       "-1",
-			}},
+		policyMap2 map[string]*Policy = map[string]*Policy{
+			testAppId: &Policy{
+				AppId: testAppId,
+				TriggerRecord: &TriggerRecord{
+					InstanceMaxCount: 5,
+					InstanceMinCount: 1,
+					ScalingRules: []*ScalingRule{
+						&ScalingRule{
+							MetricType:       "MemoryUsage",
+							StatWindow:       300,
+							BreachDuration:   300,
+							CoolDownDuration: 300,
+							Threshold:        30,
+							Operator:         "<",
+							Adjustment:       "-1",
+						},
+					},
+				},
+			},
+		}
+		policyMap3 map[string]*Policy = map[string]*Policy{
+			testAppId2: &Policy{
+				AppId: testAppId2,
+				TriggerRecord: &TriggerRecord{
+					InstanceMaxCount: 5,
+					InstanceMinCount: 1,
+					ScalingRules: []*ScalingRule{
+						&ScalingRule{
+							MetricType:       "MemoryUsage",
+							StatWindow:       300,
+							BreachDuration:   300,
+							CoolDownDuration: 300,
+							Threshold:        30,
+							Operator:         "<",
+							Adjustment:       "-1",
+						},
+					},
+				},
+			},
 		}
 	)
 	BeforeEach(func() {
@@ -80,11 +157,12 @@ var _ = Describe("AppEvaluationManager", func() {
 		fakeScalingEngine.RouteToHandler("POST", regPath, ghttp.RespondWith(http.StatusOK, "successful"))
 		database = &fakes.FakeAppMetricDB{}
 		testEvaluatorCount = 0
-		manager = NewAppEvaluationManager(testEvaluateInterval, logger, fclock, triggerArrayChan, testEvaluatorCount, database, fakeScalingEngine.URL())
 	})
 
 	Describe("Start", func() {
 		JustBeforeEach(func() {
+			manager = NewAppEvaluationManager(testEvaluateInterval, logger, fclock, triggerArrayChan, testEvaluatorCount, database,
+				fakeScalingEngine.URL(), getPolicies)
 			manager.Start()
 			Eventually(fclock.WatcherCount).Should(Equal(1))
 		})
@@ -97,8 +175,10 @@ var _ = Describe("AppEvaluationManager", func() {
 			var unBlockChan chan bool
 			var calledChan chan string
 			BeforeEach(func() {
+				getPolicies = func() map[string]*Policy {
+					return policyMap
+				}
 				testEvaluatorCount = 4
-				manager = NewAppEvaluationManager(testEvaluateInterval, logger, fclock, triggerArrayChan, testEvaluatorCount, database, fakeScalingEngine.URL())
 				unBlockChan = make(chan bool)
 				calledChan = make(chan string)
 				database.RetrieveAppMetricsStub = func(appId string, metricType string, start int64, end int64) ([]*AppMetric, error) {
@@ -107,56 +187,9 @@ var _ = Describe("AppEvaluationManager", func() {
 					<-unBlockChan
 					return nil, nil
 				}
-				testTriggerMap := map[string][]*Trigger{
-					"id1" + "#" + testMetricType: []*Trigger{&Trigger{
-						AppId:            "id1",
-						MetricType:       testMetricType,
-						BreachDuration:   300,
-						CoolDownDuration: 300,
-						Threshold:        80,
-						Operator:         ">",
-						Adjustment:       "1",
-					}},
-					"id2" + "#" + testMetricType: []*Trigger{&Trigger{
-						AppId:            "id2",
-						MetricType:       testMetricType,
-						BreachDuration:   300,
-						CoolDownDuration: 300,
-						Threshold:        80,
-						Operator:         ">",
-						Adjustment:       "1",
-					}},
-					"id3" + "#" + testMetricType: []*Trigger{&Trigger{
-						AppId:            "id3",
-						MetricType:       testMetricType,
-						BreachDuration:   300,
-						CoolDownDuration: 300,
-						Threshold:        80,
-						Operator:         ">",
-						Adjustment:       "1",
-					}},
-					"id4" + "#" + testMetricType: []*Trigger{&Trigger{
-						AppId:            "id4",
-						MetricType:       testMetricType,
-						BreachDuration:   300,
-						CoolDownDuration: 300,
-						Threshold:        80,
-						Operator:         ">",
-						Adjustment:       "1",
-					}},
-					"id5" + "#" + testMetricType: []*Trigger{&Trigger{
-						AppId:            "id5",
-						MetricType:       testMetricType,
-						BreachDuration:   300,
-						CoolDownDuration: 300,
-						Threshold:        80,
-						Operator:         ">",
-						Adjustment:       "1",
-					}},
-				}
-				manager.SetTriggers(testTriggerMap)
 			})
 			It("should create evaluatorCount evaluators", func() {
+
 				fclock.Increment(1 * testEvaluateInterval)
 				for i := 0; i < testEvaluatorCount; i++ {
 					Eventually(calledChan).Should(Receive())
@@ -173,11 +206,12 @@ var _ = Describe("AppEvaluationManager", func() {
 
 		Context("when there are triggers for evaluation", func() {
 			BeforeEach(func() {
-				manager.SetTriggers(testMap)
-
+				getPolicies = func() map[string]*Policy {
+					return policyMap2
+				}
 			})
-
 			It("should add triggers to evaluate", func() {
+
 				fclock.Increment(10 * testEvaluateInterval)
 				var arr []*Trigger
 				Eventually(triggerArrayChan).Should(Receive(&arr))
@@ -186,14 +220,6 @@ var _ = Describe("AppEvaluationManager", func() {
 					MetricType:       testMetricType,
 					BreachDuration:   300,
 					CoolDownDuration: 300,
-					Threshold:        80,
-					Operator:         ">",
-					Adjustment:       "1",
-				}, &Trigger{
-					AppId:            testAppId,
-					MetricType:       testMetricType,
-					BreachDuration:   300,
-					CoolDownDuration: 300,
 					Threshold:        30,
 					Operator:         "<",
 					Adjustment:       "-1",
@@ -201,21 +227,28 @@ var _ = Describe("AppEvaluationManager", func() {
 			})
 		})
 
-		Context("when there is trigger", func() {
+		Context("when there is no trigger", func() {
 			BeforeEach(func() {
-				manager.SetTriggers(map[string][]*Trigger{})
+				getPolicies = func() map[string]*Policy {
+					return nil
+				}
 			})
 
 			It("should add no trigger to evaluate", func() {
+				fclock.Increment(10 * testEvaluateInterval)
 				Consistently(triggerArrayChan).ShouldNot(Receive())
 			})
 		})
 		Context("when triggers are changed", func() {
+			var resultPolicyMap map[string]*Policy
 			BeforeEach(func() {
-				manager.SetTriggers(testMap)
+				getPolicies = func() map[string]*Policy {
+					return resultPolicyMap
+				}
 			})
-
 			It("should add new triggers to evaluate", func() {
+
+				resultPolicyMap = policyMap2
 				fclock.Increment(1 * testEvaluateInterval)
 				var arr []*Trigger
 				Eventually(triggerArrayChan).Should(Receive(&arr))
@@ -225,32 +258,15 @@ var _ = Describe("AppEvaluationManager", func() {
 					MetricType:       testMetricType,
 					BreachDuration:   300,
 					CoolDownDuration: 300,
-					Threshold:        80,
-					Operator:         ">",
-					Adjustment:       "1",
-				}, &Trigger{
-					AppId:            testAppId,
-					MetricType:       testMetricType,
-					BreachDuration:   300,
-					CoolDownDuration: 300,
 					Threshold:        30,
 					Operator:         "<",
 					Adjustment:       "-1",
 				}}))
-
-				manager.SetTriggers(testMap2)
+				resultPolicyMap = policyMap3
 				fclock.Increment(1 * testEvaluateInterval)
 				Eventually(triggerArrayChan).Should(Receive(&arr))
 
 				Expect(arr).To(Equal([]*Trigger{&Trigger{
-					AppId:            testAppId2,
-					MetricType:       testMetricType,
-					BreachDuration:   300,
-					CoolDownDuration: 300,
-					Threshold:        80,
-					Operator:         ">",
-					Adjustment:       "1",
-				}, &Trigger{
 					AppId:            testAppId2,
 					MetricType:       testMetricType,
 					BreachDuration:   300,
@@ -266,11 +282,15 @@ var _ = Describe("AppEvaluationManager", func() {
 
 	Describe("Stop", func() {
 		BeforeEach(func() {
+			getPolicies = func() map[string]*Policy {
+				return policyMap
+			}
+			manager = NewAppEvaluationManager(testEvaluateInterval, logger, fclock, triggerArrayChan, testEvaluatorCount, database,
+				fakeScalingEngine.URL(), getPolicies)
 			manager.Start()
 			Eventually(fclock.WatcherCount).Should(Equal(1))
 			manager.Stop()
 			Eventually(fclock.WatcherCount).Should(Equal(0))
-			manager.SetTriggers(testMap)
 		})
 
 		It("stops adding triggers to evaluate ", func() {
