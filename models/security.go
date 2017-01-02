9e85952f4fac815ea486fde21be86b87c4608413
qiyangbj@cn.ibm.com <qiyangbj@cn.ibm.com>
2017-01-03 00:09:44 +0800

    update broker-api integration test to adapt broker's tls support.
    [#136717867]

--- models/security.go
+++ models/security.go
@@ -1,7 +1,7 @@
 package models
 
 type TLSCerts struct {
-	KeyFile    string `yaml:"key_file"`
-	CertFile   string `yaml:"cert_file"`
-	CACertFile string `yaml:"ca_file"`
+	KeyFile    string `yaml:"key_file" json:"keyFile"`
+	CertFile   string `yaml:"cert_file" json:"certFile"`
+	CACertFile string `yaml:"ca_file" json:"caCertFile"`
 }
