868229c32475e83b20711795a3a0e15a1448a37b
kanekoh <hiroyukik@fast.au.fujitsu.com>
2017-03-02 10:16:35 +1100

    Fixed properties and messages in scheduler
    [#140540769]

--- scheduler/src/test/java/org/cloudfoundry/autoscaler/scheduler/util/TestConfiguration.java
+++ scheduler/src/test/java/org/cloudfoundry/autoscaler/scheduler/util/TestConfiguration.java
@@ -2,9 +2,12 @@ package org.cloudfoundry.autoscaler.scheduler.util;
 
 import org.springframework.test.context.TestPropertySource;
 
-@TestPropertySource(properties = { "scalingenginejob.reschedule.interval.millisecond=100",
-		"scalingenginejob.reschedule.maxcount=5", "autoscaler.scalingengine.url=https://localhost:8091",
+@TestPropertySource(properties = { "spring.datasource.driverClassName=org.postgresql.Driver",
+		"spring.datasource.url=jdbc:postgresql://127.0.0.1/autoscaler", "spring.datasource.username=postgres",
+		"spring.datasource.password=postgres", "scalingenginejob.reschedule.interval.millisecond=100",
+		"scalingenginejob.reschedule.maxcount=5",
 		"scalingengine.notification.reschedule.maxcount=2",
+		"autoscaler.scalingengine.url=https://localhost:8091",
 		"server.ssl.key-store=src/test/resources/certs/test-scheduler.p12",
 		"server.ssl.key-alias=test-scheduler",
 		"server.ssl.key-store-password=123456",
@@ -14,6 +17,6 @@ import org.springframework.test.context.TestPropertySource;
 		"client.ssl.key-store-type=PKCS12",
 		"client.ssl.trust-store=src/test/resources/certs/test.truststore",
 		"client.ssl.trust-store-password=123456",
-		"client.ssl.protocol=TLSv1.2"})
+		"client.ssl.protocol=TLSv1.2", "org.quartz.scheduler.instanceName=app-autoscaler" })
 public class TestConfiguration {
 }
