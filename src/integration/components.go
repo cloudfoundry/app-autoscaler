9b1233aa3d9c0d05fa74ac8d1b9dd4066962fad0
kanekoh <hiroyukik@fast.au.fujitsu.com>
2017-03-09 15:37:19 +1100

    Applied review comment.
    [#140444445]

--- src/integration/components.go
+++ src/integration/components.go
@@ -103,7 +103,7 @@ func (components *Components) Scheduler(confPath string, argv ...string) *ginkgo
 	return ginkgomon.New(ginkgomon.Config{
 		Name:              Scheduler,
 		AnsiColorCode:     "34m",
-		StartCheck:        "Tomcat started on port",
+		StartCheck:        "Started SchedulerApplication in",
 		StartCheckTimeout: 120 * time.Second,
 		Command: exec.Command(
 			"java", append([]string{"-jar", "-Dspring.config.location=" + confPath, "-Dserver.port=" + strconv.FormatInt(int64(components.Ports[Scheduler]), 10), components.Executables[Scheduler]}, argv...)...,
