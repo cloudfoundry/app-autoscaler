5b8c8574318536e2e2bf8e297e4d0104e82dea0e
Tanmoy Pal <tanmoy.pal@sap.com>
2017-03-16 17:51:44 +0000

    [#140730323] Added consul based lock for Pruner (#199)
    * Added consul based lock for pruner

--- pruner/cmd/pruner/pruner_test.go
+++ pruner/cmd/pruner/pruner_test.go
@@ -1,50 +1,145 @@
 package main_test
 
 import (
+	"autoscaler/pruner"
+	"io/ioutil"
+	"os"
+	"time"
+
+	"code.cloudfoundry.org/clock"
+	"code.cloudfoundry.org/consuladapter"
+	"code.cloudfoundry.org/lager/lagertest"
+	"code.cloudfoundry.org/locket"
+	"github.com/hashicorp/consul/api"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	. "github.com/onsi/gomega/gbytes"
 	. "github.com/onsi/gomega/gexec"
-	"io/ioutil"
-	"os"
+	"github.com/tedsuo/ifrit"
+	"github.com/tedsuo/ifrit/ginkgomon"
 )
 
 var _ = Describe("Pruner", func() {
-	var runner *PrunerRunner
 
+	var (
+		runner       *PrunerRunner
+		consulClient consuladapter.Client
+	)
 	BeforeEach(func() {
 		initConfig()
+		consulRunner.Reset()
+		consulClient = consulRunner.NewClient()
 		runner = NewPrunerRunner()
-	})
-
-	JustBeforeEach(func() {
-		runner.Start()
 	})
 
 	AfterEach(func() {
 		runner.KillWithFire()
 	})
 
-	Context("when pruner started", func() {
-		It("should start instancemetrics dbpruner", func() {
-			Eventually(runner.Session).Should(Say("instancemetrics-dbpruner.started"))
-			Consistently(runner.Session).ShouldNot(Exit())
+	Context("when the pruner acquires the lock", func() {
+		BeforeEach(func() {
+			runner.startCheck = ""
+			runner.Start()
+			Eventually(runner.Session.Buffer, 2*time.Second).Should(Say(runner.acquiredLockCheck))
 		})
 
-		It("should start appmetrics dbpruner", func() {
-			Eventually(runner.Session).Should(Say("appmetrics-dbpruner.started"))
-			Consistently(runner.Session).ShouldNot(Exit())
+		It("registers itself with consul", func() {
+			Eventually(runner.Session.Buffer, 2*time.Second).Should(Say("pruner.registration-runner.succeeded-registering-service"))
+
+			services, err := consulClient.Agent().Services()
+			Expect(err).ToNot(HaveOccurred())
+
+			Expect(services).To(HaveKeyWithValue("pruner",
+				&api.AgentService{
+					Service: "pruner",
+					ID:      "pruner",
+				}))
 		})
 
-		It("should start scalingengine dbpruner", func() {
-			Eventually(runner.Session).Should(Say("scalingengine-dbpruner.started"))
+		It("registers a TTL healthcheck", func() {
+			Eventually(runner.Session.Buffer, 2*time.Second).Should(Say("pruner.registration-runner.succeeded-registering-service"))
+
+			checks, err := consulClient.Agent().Checks()
+			Expect(err).ToNot(HaveOccurred())
+
+			Expect(checks).To(HaveKeyWithValue("service:pruner",
+				&api.AgentCheck{
+					Node:        "0",
+					CheckID:     "service:pruner",
+					Name:        "Service 'pruner' check",
+					Status:      "passing",
+					ServiceID:   "pruner",
+					ServiceName: "pruner",
+				}))
+		})
+
+		It("should start", func() {
+			Eventually(runner.Session.Buffer, 2*time.Second).Should(Say("pruner.instancemetrics-dbpruner.started"))
+			Eventually(runner.Session.Buffer, 2*time.Second).Should(Say("pruner.appmetrics-dbpruner.started"))
+			Eventually(runner.Session.Buffer, 2*time.Second).Should(Say("pruner.scalingengine-dbpruner.started"))
+			Eventually(runner.Session.Buffer, 2*time.Second).Should(Say("pruner.started"))
 			Consistently(runner.Session).ShouldNot(Exit())
+		})
+	})
+
+	Context("when the pruner loses the lock", func() {
+		BeforeEach(func() {
+			runner.startCheck = ""
+			runner.Start()
+
+			Eventually(runner.Session.Buffer, 2*time.Second).Should(Say(runner.acquiredLockCheck))
+			Eventually(runner.Session.Buffer, 2*time.Second).Should(Say("pruner.started"))
+
+			consulRunner.Reset()
+		})
+
+		It("exits with failure", func() {
+			Eventually(runner.Session, 4*time.Second).Should(Exit(1))
+			Expect(runner.Session.Buffer()).Should(Say("exited-with-failure"))
+		})
+	})
+
+	Context("when the pruner initially does not have the lock", func() {
+		var competingPrunerProcess ifrit.Process
+
+		BeforeEach(func() {
+			logger := lagertest.NewTestLogger("competing-process")
+			buffer := logger.Buffer()
+
+			competingPrunerLock := locket.NewLock(logger, consulClient, pruner.PrunerLockSchemaPath(), []byte{}, clock.NewClock(), cfg.Lock.LockRetryInterval, cfg.Lock.LockTTL)
+			competingPrunerProcess = ifrit.Invoke(competingPrunerLock)
+			Eventually(buffer, 2*time.Second).Should(Say("competing-process.lock.acquire-lock-succeeded"))
+
+			runner.startCheck = ""
+			runner.Start()
+		})
+
+		It("should not start", func() {
+			Eventually(runner.Session.Buffer).Should(Say("pruner.lock.acquiring-lock"))
+			Consistently(runner.Session.Buffer).ShouldNot(Say("pruner.started"))
+		})
+
+		Describe("when the lock becomes available", func() {
+			BeforeEach(func() {
+				ginkgomon.Kill(competingPrunerProcess)
+			})
+
+			It("acquires the lock and starts", func() {
+				Eventually(runner.Session.Buffer, 2*time.Second).Should(Say(runner.acquiredLockCheck))
+				Eventually(runner.Session.Buffer, 2*time.Second).Should(Say("pruner.instancemetrics-dbpruner.started"))
+				Eventually(runner.Session.Buffer, 2*time.Second).Should(Say("pruner.appmetrics-dbpruner.started"))
+				Eventually(runner.Session.Buffer, 2*time.Second).Should(Say("pruner.scalingengine-dbpruner.started"))
+				Eventually(runner.Session.Buffer, 2*time.Second).Should(Say("pruner.started"))
+				Consistently(runner.Session).ShouldNot(Exit())
+			})
+
 		})
 	})
 
 	Context("with a missing config file", func() {
 		BeforeEach(func() {
 			runner.configPath = "bogus"
+			runner.Start()
 		})
 
 		It("fails with an error", func() {
@@ -59,6 +154,7 @@ var _ = Describe("Pruner", func() {
 			Expect(err).NotTo(HaveOccurred())
 			runner.configPath = badfile.Name()
 			ioutil.WriteFile(runner.configPath, []byte("bogus"), os.ModePerm)
+			runner.Start()
 		})
 
 		AfterEach(func() {
@@ -78,6 +174,7 @@ var _ = Describe("Pruner", func() {
 
 			cfg := writeConfig(&cfg)
 			runner.configPath = cfg.Name()
+			runner.Start()
 		})
 
 		AfterEach(func() {
@@ -95,6 +192,7 @@ var _ = Describe("Pruner", func() {
 			cfg.InstanceMetricsDb.DbUrl = "postgres://not-exist-user:not-exist-password@localhost/autoscaler?sslmode=disable"
 			cfg := writeConfig(&cfg)
 			runner.configPath = cfg.Name()
+			runner.Start()
 		})
 
 		AfterEach(func() {
@@ -113,6 +211,7 @@ var _ = Describe("Pruner", func() {
 			cfg.AppMetricsDb.DbUrl = "postgres://not-exist-user:not-exist-password@localhost/autoscaler?sslmode=disable"
 			cfg := writeConfig(&cfg)
 			runner.configPath = cfg.Name()
+			runner.Start()
 		})
 
 		AfterEach(func() {
@@ -131,6 +230,7 @@ var _ = Describe("Pruner", func() {
 			cfg.ScalingEngineDb.DbUrl = "postgres://not-exist-user:not-exist-password@localhost/autoscaler?sslmode=disable"
 			cfg := writeConfig(&cfg)
 			runner.configPath = cfg.Name()
+			runner.Start()
 		})
 
 		AfterEach(func() {
@@ -145,6 +245,10 @@ var _ = Describe("Pruner", func() {
 	})
 
 	Context("when an interrupt is sent", func() {
+		BeforeEach(func() {
+			runner.Start()
+		})
+
 		It("should stop", func() {
 			runner.Interrupt()
 			Eventually(runner.Session, 5).Should(Exit(130))
--- pruner/config/config_test.go
+++ pruner/config/config_test.go
@@ -42,6 +42,8 @@ scaling_engine_db:
   db_url: "postgres://postgres:postgres@localhost/autoscaler?sslmode=disable"
   refresh_interval: 36h
   cutoff_days: 30
+lock:
+  consul_cluster_config: "http://127.0.0.1:8500"
 `)
 			})
 
@@ -67,6 +69,8 @@ scaling_engine_db:
   db_url: "postgres://postgres:postgres@localhost/autoscaler?sslmode=disable"
   refresh_interval: 36h
   cutoff_days: 30s
+lock:
+  consul_cluster_config: "http://127.0.0.1:8500"
 `)
 			})
 
@@ -92,6 +96,10 @@ scaling_engine_db:
   db_url: "postgres://postgres:postgres@localhost/autoscaler?sslmode=disable"
   refresh_interval: 36h
   cutoff_days: 30
+lock:
+  lock_ttl: 15s
+  lock_retry_interval: 10s
+  consul_cluster_config: "http://127.0.0.1:8500"
 `)
 			})
 
@@ -112,6 +120,10 @@ scaling_engine_db:
 				Expect(conf.ScalingEngineDb.RefreshInterval).To(Equal(36 * time.Hour))
 				Expect(conf.ScalingEngineDb.CutoffDays).To(Equal(30))
 
+				Expect(conf.Lock.LockTTL).To(Equal(15 * time.Second))
+				Expect(conf.Lock.LockRetryInterval).To(Equal(10 * time.Second))
+				Expect(conf.Lock.ConsulClusterConfig).To(Equal("http://127.0.0.1:8500"))
+
 			})
 		})
 
@@ -123,7 +135,9 @@ instance_metrics_db:
 app_metrics_db:
   db_url: "postgres://postgres:postgres@localhost/autoscaler?sslmode=disable"
 scaling_engine_db:
-  db_url: "postgres://postgres:postgres@localhost/autoscaler?sslmode=disable"  
+  db_url: "postgres://postgres:postgres@localhost/autoscaler?sslmode=disable" 
+lock:
+  consul_cluster_config: "http://127.0.0.1:8500" 
 `)
 			})
 
@@ -140,6 +154,68 @@ scaling_engine_db:
 
 				Expect(conf.ScalingEngineDb.RefreshInterval).To(Equal(config.DefaultRefreshInterval))
 				Expect(conf.ScalingEngineDb.CutoffDays).To(Equal(config.DefaultCutoffDays))
+
+				Expect(conf.Lock.ConsulClusterConfig).To(Equal("http://127.0.0.1:8500"))
+				Expect(conf.Lock.LockTTL).To(Equal(config.DefaultLockTTL))
+				Expect(conf.Lock.LockRetryInterval).To(Equal(config.DefaultRetryInterval))
+			})
+		})
+
+		Context("when it gives a non integer lock ttl", func() {
+			BeforeEach(func() {
+				configBytes = []byte(`
+logging:
+  level: "debug"
+instance_metrics_db:
+  db_url: "postgres://postgres:postgres@localhost/autoscaler?sslmode=disable"
+  refresh_interval: 12h
+  cutoff_days: 10
+app_metrics_db:
+  db_url: "postgres://postgres:postgres@localhost/autoscaler?sslmode=disable"
+  refresh_interval: 24h
+  cutoff_days: 20
+scaling_engine_db:
+  db_url: "postgres://postgres:postgres@localhost/autoscaler?sslmode=disable"
+  refresh_interval: 36h
+  cutoff_days: 30s
+lock:
+  lock_ttl: NON-INTEGER-VALUE
+  lock_retry_interval: 10s
+  consul_cluster_config: "http://127.0.0.1:8500"
+`)
+			})
+
+			It("should error", func() {
+				Expect(err).To(MatchError(MatchRegexp("cannot unmarshal .* into time.Duration")))
+			})
+		})
+
+		Context("when it gives a non integer lock retry interval", func() {
+			BeforeEach(func() {
+				configBytes = []byte(`
+logging:
+  level: "debug"
+instance_metrics_db:
+  db_url: "postgres://postgres:postgres@localhost/autoscaler?sslmode=disable"
+  refresh_interval: 12h
+  cutoff_days: 10
+app_metrics_db:
+  db_url: "postgres://postgres:postgres@localhost/autoscaler?sslmode=disable"
+  refresh_interval: 24h
+  cutoff_days: 20
+scaling_engine_db:
+  db_url: "postgres://postgres:postgres@localhost/autoscaler?sslmode=disable"
+  refresh_interval: 36h
+  cutoff_days: 30s
+lock:
+  lock_ttl: 15s
+  lock_retry_interval: NON-INTEGER-VALUE
+  consul_cluster_config: "http://127.0.0.1:8500"
+`)
+			})
+
+			It("should error", func() {
+				Expect(err).To(MatchError(MatchRegexp("cannot unmarshal .* into time.Duration")))
 			})
 		})
 
@@ -160,6 +236,10 @@ scaling_engine_db:
 			conf.ScalingEngineDb.DbUrl = "postgres://pqgotest:password@exampl.com/pqgotest"
 			conf.ScalingEngineDb.RefreshInterval = 36 * time.Hour
 			conf.ScalingEngineDb.CutoffDays = 20
+
+			conf.Lock.LockTTL = 15 * time.Second
+			conf.Lock.LockRetryInterval = 10 * time.Second
+			conf.Lock.ConsulClusterConfig = "http://127.0.0.1:8500"
 
 		})
 
@@ -269,6 +349,39 @@ scaling_engine_db:
 
 			It("should error", func() {
 				Expect(err).To(MatchError(MatchRegexp("Configuration error: ScalingEngine DB cutoff days is negative")))
+			})
+		})
+
+		Context("when Lock ttl value in seconds is set to a negative value", func() {
+
+			BeforeEach(func() {
+				conf.Lock.LockTTL = -10
+			})
+
+			It("should error", func() {
+				Expect(err).To(MatchError(MatchRegexp("Configuration error: lock ttl is less than or equal to 0")))
+			})
+		})
+
+		Context("when Lock retry interval value in seconds is set to a negative value", func() {
+
+			BeforeEach(func() {
+				conf.Lock.LockRetryInterval = -15
+			})
+
+			It("should error", func() {
+				Expect(err).To(MatchError(MatchRegexp("Configuration error: lock retry interval is less than or equal to 0")))
+			})
+		})
+
+		Context("when Consul Cluster Config is not set", func() {
+
+			BeforeEach(func() {
+				conf.Lock.ConsulClusterConfig = ""
+			})
+
+			It("should error", func() {
+				Expect(err).To(MatchError(MatchRegexp("Configuration error: Consul Cluster Config is empty")))
 			})
 		})
 
