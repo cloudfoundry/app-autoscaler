8c61504e9432618282b8099d7c013950d23f1c65
qibobo <lqiyangl@gmail.com>
2017-07-27 21:58:25 +0800

    nothing

--- servicebroker/lib/routes/binding.js
+++ servicebroker/lib/routes/binding.js
@@ -178,7 +178,7 @@ module.exports = function(app, settings, catalog) {
       });
     }).catch(function(error2) {
       logger.error("Unbind failed: transaction error", { error: error2 });
-      rollbackTransaction(t, res, 500, {});
+      rollbackTransaction(t, res, 500, {}); 
     });
   });
 }
