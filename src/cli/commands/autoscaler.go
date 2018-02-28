79620de64e187e74943d4db200a455319e18e295
cdlliuy <cdlliuy@cn.ibm.com>
2018-02-28 15:54:39 +0800

    CLI implementation of "autoscaling-history" (#299)
    *  CLI implementation of "autoscaling-history"
    
         * [#117085177] Taks 7: implementation of “autoscaling-history”
    
    * fix help info in Readme and main.go
    
    * update per comments
    
    * further updates per comments

--- src/cli/commands/autoscaler.go
+++ src/cli/commands/autoscaler.go
@@ -12,6 +12,7 @@ type AutoScalerCmds struct {
 	AttachPolicy AttachPolicyCommand `command:"attach-autoscaling-policy" description:"Attach a scaling policy to an application"`
 	DetachPolicy DetachPolicyCommand `command:"detach-autoscaling-policy" description:"Detach a scaling policy from an application"`
 	Metrics      MetricsCommand      `command:"autoscaling-metrics" description:"Retrieve the metrics of an application"`
+	History      HistoryCommand      `command:"autoscaling-history" description:"Retrieve the history of an application"`
 
 	UninstallPlugin UninstallHook `command:"CLI-MESSAGE-UNINSTALL"`
 }
---
+++ src/cli/util/time/parse.go
@@ -0,0 +1,30 @@
+package time
+
+import (
+	"cli/ui"
+	"errors"
+	"fmt"
+	"time"
+)
+
+var timeFormats = []string{
+	"2006-01-02T15:04:05Z",
+	"2006-01-02T15:04:05-07:00",
+}
+
+func ParseTimeFormat(input string) (ns int64, e error) {
+
+	if input != "" {
+		for _, format := range timeFormats {
+			t, e := time.Parse(format, input)
+			if e == nil {
+				ns = t.UnixNano()
+				if ns >= 0 {
+					return ns, nil
+				}
+			}
+		}
+	}
+	e = errors.New(fmt.Sprintf(ui.UnrecognizedTimeFormat, input))
+	return
+}
