cee4d0b4ea175d200da21fd2e78d0e17863af341
paltanmoy <tanmoy.pal@sap.com>
2018-03-21 13:57:31 +0530

    Added schema validation

--- servicebroker/test/routes/catalog_test.js
+++ servicebroker/test/routes/catalog_test.js
@@ -2,6 +2,7 @@
 var supertest = require("supertest");
 var expect = require('chai').expect;
 
+var Ajv = require('ajv');
 var fs = require('fs');
 var path = require('path');
 var BrokerServer = require(path.join(__dirname, '../../lib/server.js'));
@@ -10,9 +11,13 @@ var settings = require(path.join(__dirname, '../../lib/config/setting.js'))((JSO
   fs.readFileSync(configFilePath, 'utf8'))));
 var auth = new Buffer(settings.username + ":" + settings.password).toString('base64');
 var catalog = JSON.parse(fs.readFileSync(path.join(__dirname, '../../config/catalog.json'), 'utf8'));
+var fakeValidPolicy = JSON.parse(fs.readFileSync(path.join(__dirname, '../fakeValidPolicy.json'), 'utf8'));
+var fakeInvalidPolicy = JSON.parse(fs.readFileSync(path.join(__dirname, '../fakeInvalidPolicy.json'), 'utf8'));
+var ajv = new Ajv();
+ajv.addMetaSchema(require('ajv/lib/refs/json-schema-draft-06.json'));
 
 describe('getCatalog RESTful API', function() {
-  var servers, publicServer, internalServer;
+  var servers, publicServer, internalServer, validate;
   beforeEach(function() {
     servers = BrokerServer(settings, catalog, function(){});
     publicServer = servers.publicServer;
@@ -26,17 +31,18 @@ describe('getCatalog RESTful API', function() {
   });
 
   it("should return catalog json", function(done) {
-
     supertest(publicServer)
       .get("/v2/catalog")
       .set("Authorization", "Basic " + auth)
       .expect(200)
       .expect("Content-type", /json/)
       .end(function(err, res) {
-        expect(res.body.services[0].plans[0]).to.have.property('schemas');
         expect(JSON.stringify(res.body)).to.be.equal(JSON.stringify(catalog));
+        expect(res.body.services[0].plans[0]).to.have.property('schemas');
+        validate = ajv.compile(res.body.services[0].plans[0].schemas.service_binding.create.parameters);
+        expect(validate(fakeValidPolicy)).to.be.true;
+        expect(validate(fakeInvalidPolicy)).to.be.false;
         done();
       });
   });
-
 });
