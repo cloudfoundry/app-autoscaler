40ad232e0268246978df556602e60a1908b7436a
qibobo <qiyangbj@cn.ibm.com>
2018-12-29 14:07:15 +0800

    emit envelops to metric server

--- src/autoscaler/metricsgateway/emitter.go
+++ src/autoscaler/metricsgateway/emitter.go
@@ -1,8 +1,17 @@
 package metricsgateway
 
 import (
+	"crypto/tls"
+	"net/http"
+	"sync"
+	"time"
+
+	"autoscaler/routes"
+
 	"code.cloudfoundry.org/go-loggregator/rpc/loggregator_v2"
 	"code.cloudfoundry.org/lager"
+	"github.com/golang/protobuf/proto"
+	"github.com/gorilla/websocket"
 )
 
 type Emitter interface {
@@ -10,27 +19,43 @@ type Emitter interface {
 	Emit(*loggregator_v2.Envelope) error
 }
 
-type emitter struct {
+type EnvelopeEmitter struct {
 	logger               lager.Logger
 	metricsServerAddress string
+	handshakeTimeout     time.Duration
 	envelopChan          chan *loggregator_v2.Envelope
 	bufferSize           int64
 	doneChan             chan bool
+	wsConn               *websocket.Conn
+	lock                 sync.Mutex
+	dialer               websocket.Dialer
 }
 
-func NewEmitter(logger lager.Logger, bufferSize int64, metricsServerAddress string) Emitter {
-	return &emitter{
-		logger:               logger.Session("Emitter"),
+func NewEnvelopeEmitter(logger lager.Logger, bufferSize int64, metricsServerAddress string, tlsConfig *tls.Config, handshakeTimeout time.Duration) *EnvelopeEmitter {
+	return &EnvelopeEmitter{
+		logger:               logger.Session("EnvelopeEmitter"),
 		metricsServerAddress: metricsServerAddress,
+		handshakeTimeout:     handshakeTimeout,
 		envelopChan:          make(chan *loggregator_v2.Envelope, bufferSize),
+		doneChan:             make(chan bool),
+		dialer: websocket.Dialer{
+			TLSClientConfig:  tlsConfig,
+			Proxy:            http.ProxyFromEnvironment,
+			HandshakeTimeout: handshakeTimeout,
+		},
 	}
 }
-func (e *emitter) Start() {
+func (e *EnvelopeEmitter) Start() {
+	err := e.setupWSConn()
+	if err != nil {
+		e.logger.Error("failed-to-start-emimtter", err)
+		return
+	}
 	go e.startEmitEnvelope()
 	e.logger.Info("started")
 }
 
-func (e *emitter) startEmitEnvelope() {
+func (e *EnvelopeEmitter) startEmitEnvelope() {
 	for {
 		select {
 		case <-e.doneChan:
@@ -45,15 +70,42 @@ func (e *emitter) startEmitEnvelope() {
 	}
 }
 
-func (e *emitter) Stop() {
+func (e *EnvelopeEmitter) Stop() {
+	err := e.closeWSConn()
+	if err != nil {
+		e.logger.Error("failed-to-close-ws-connection", err)
+	}
 	e.doneChan <- true
 
 }
 
-func (e *emitter) Accept(envelope *loggregator_v2.Envelope) {
+func (e *EnvelopeEmitter) Accept(envelope *loggregator_v2.Envelope) {
+	e.logger.Debug("accept-envelope", lager.Data{"envelope": envelope})
 	e.envelopChan <- envelope
 }
-func (e *emitter) Emit(*loggregator_v2.Envelope) error {
-	// to be done
+func (e *EnvelopeEmitter) Emit(envelope *loggregator_v2.Envelope) error {
+	e.logger.Debug("emit-envelope", lager.Data{"envelope": envelope})
+	bytes, err := proto.Marshal(envelope)
+	if err != nil {
+		return err
+	}
+	err = e.wsConn.WriteMessage(websocket.BinaryMessage, bytes)
+	return err
+}
+
+func (e *EnvelopeEmitter) setupWSConn() error {
+	e.logger.Info("setup-new-ws-connection")
+	con, _, err := e.dialer.Dial(e.metricsServerAddress+routes.EnvelopePath, nil)
+	if err != nil {
+		e.logger.Error("failed-to-create-websocket-connection-to-metricserver", err, lager.Data{"metricServerUrl": (e.metricsServerAddress + routes.EnvelopePath)})
+		return err
+	}
+	e.lock.Lock()
+	defer e.lock.Unlock()
+	e.wsConn = con
 	return nil
+}
+func (e *EnvelopeEmitter) closeWSConn() error {
+	e.logger.Info("close-ws-connection")
+	return e.wsConn.WriteControl(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""), time.Time{})
 }
---
+++ src/autoscaler/metricsgateway/emitter_test.go
@@ -0,0 +1,104 @@
+package metricsgateway_test
+
+import (
+	. "autoscaler/metricsgateway"
+	"autoscaler/metricsgateway/testhelpers"
+	"strings"
+	"time"
+
+	"code.cloudfoundry.org/go-loggregator/rpc/loggregator_v2"
+	"code.cloudfoundry.org/lager/lagertest"
+	"github.com/gorilla/websocket"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	. "github.com/onsi/gomega/gbytes"
+	"github.com/onsi/gomega/ghttp"
+)
+
+var _ = Describe("Emitter", func() {
+	var (
+		logger               *lagertest.TestLogger
+		fakeMetricServer     *ghttp.Server
+		metricServerAddress  string
+		messageChan          chan []byte
+		messageTypeChan      chan int
+		bufferSize           int64 = 500
+		emitter              *EnvelopeEmitter
+		testAppId            = "test-app-id"
+		testHandshakeTimeout = 5 * time.Second
+
+		testEnvelope = loggregator_v2.Envelope{
+			SourceId: testAppId,
+			Message: &loggregator_v2.Envelope_Gauge{
+				Gauge: &loggregator_v2.Gauge{
+					Metrics: map[string]*loggregator_v2.GaugeValue{
+						"cpu": &loggregator_v2.GaugeValue{
+							Unit:  "percentage",
+							Value: 20.5,
+						},
+						"disk": &loggregator_v2.GaugeValue{
+							Unit:  "bytes",
+							Value: 3000000000,
+						},
+						"memory": &loggregator_v2.GaugeValue{
+							Unit:  "bytes",
+							Value: 1000000000,
+						},
+						"memory_quota": &loggregator_v2.GaugeValue{
+							Unit:  "bytes",
+							Value: 2000000000,
+						},
+					},
+				},
+			},
+		}
+	)
+	BeforeEach(func() {
+		logger = lagertest.NewTestLogger("emitter")
+		fakeMetricServer = ghttp.NewServer()
+		metricServerAddress = strings.Replace(fakeMetricServer.URL(), "http", "ws", 1)
+		messageChan = make(chan []byte, 10)
+		messageTypeChan = make(chan int, 10)
+		wsh := testhelpers.NewWebsocketHandler(messageChan, messageTypeChan, 100*time.Second)
+
+		fakeMetricServer.RouteToHandler("GET", "/v1/envelopes", wsh.ServeWebsocket)
+	})
+	Context("Start", func() {
+		JustBeforeEach(func() {
+			emitter = NewEnvelopeEmitter(logger, bufferSize, metricServerAddress, nil, testHandshakeTimeout)
+			emitter.Start()
+			emitter.Accept(&testEnvelope)
+		})
+		It("should emit envelops to metricServer", func() {
+			Eventually(messageChan).Should(Receive())
+		})
+
+		Context("when metricServer is not started", func() {
+			BeforeEach(func() {
+				fakeMetricServer.Close()
+			})
+			It("failed to start emitter", func() {
+				Consistently(messageChan).ShouldNot(Receive())
+				Eventually(logger.Buffer).Should(Say("failed-to-start-emimtter"))
+			})
+		})
+	})
+
+	Context("Stop", func() {
+		BeforeEach(func() {
+			emitter = NewEnvelopeEmitter(logger, bufferSize, metricServerAddress, nil, testHandshakeTimeout)
+			emitter.Start()
+			Eventually(logger.Buffer).Should(Say("started"))
+			emitter.Accept(&testEnvelope)
+			Eventually(messageChan).Should(Receive())
+			emitter.Stop()
+			By("should close ws connection")
+			Eventually(messageTypeChan).Should(Receive(Equal(websocket.CloseMessage)))
+
+			emitter.Accept(&testEnvelope)
+		})
+		It("should stop emitting envelope", func() {
+			Consistently(messageChan).ShouldNot(Receive())
+		})
+	})
+})
