9944e3cf9f678ec2dd7bae7486982f76acfb2133
Bo Yang <byang@us.ibm.com>
2019-01-06 11:05:43 -0800

    refactor metrics server

--- src/autoscaler/metricsserver/collector/ws_server.go
+++ src/autoscaler/metricsserver/collector/ws_server.go
@@ -2,8 +2,9 @@ package collector
 
 import (
 	"autoscaler/healthendpoint"
-	"autoscaler/metricsserver/config"
+	"autoscaler/models"
 	"fmt"
+	"time"
 
 	"code.cloudfoundry.org/cfhttp"
 	"code.cloudfoundry.org/go-loggregator/rpc/loggregator_v2"
@@ -12,22 +13,22 @@ import (
 	"github.com/tedsuo/ifrit/http_server"
 )
 
-func NewWSServer(logger lager.Logger, conf *config.Config, envelopeChannels []chan *loggregator_v2.Envelope, httpStatusCollector healthendpoint.HTTPStatusCollector) (ifrit.Runner, error) {
-	wsHandler := NewWSMessageHandler(logger.Session("ws_handler"), envelopeChannels, conf.Collector.KeepAliveTime)
-	addr := fmt.Sprintf("0.0.0.0:%d", conf.Server.Port)
+func NewWSServer(logger lager.Logger, tls models.TLSCerts, port int, keepAlive time.Duration, envelopeChannels []chan *loggregator_v2.Envelope, httpStatusCollector healthendpoint.HTTPStatusCollector) (ifrit.Runner, error) {
+	wsHandler := NewWSMessageHandler(logger.Session("ws_handler"), envelopeChannels, keepAlive)
+	addr := fmt.Sprintf("0.0.0.0:%d", port)
 
 	var runner ifrit.Runner
-	if (conf.Server.TLS.KeyFile == "") || (conf.Server.TLS.CertFile == "") {
+	if (tls.KeyFile == "") || (tls.CertFile == "") {
 		runner = http_server.New(addr, wsHandler)
 
 	} else {
-		tlsConfig, err := cfhttp.NewTLSConfig(conf.Server.TLS.CertFile, conf.Server.TLS.KeyFile, conf.Server.TLS.CACertFile)
+		tlsConfig, err := cfhttp.NewTLSConfig(tls.CertFile, tls.KeyFile, tls.CACertFile)
 		if err != nil {
-			logger.Error("failed-new-websocket-server-new-tls-config", err, lager.Data{"tls": conf.Server.TLS})
+			logger.Error("failed-new-websocket-server-new-tls-config", err)
 			return nil, err
 		}
 		runner = http_server.NewTLSServer(addr, wsHandler, tlsConfig)
 	}
-	logger.Info("websocket-server-created", lager.Data{"port": conf.Server.Port})
+	logger.Info("websocket-server-created", lager.Data{"port": port})
 	return runner, nil
 }
