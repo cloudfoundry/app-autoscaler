d858dda95f46fc35bf9d6c1d7603d573c8f5c547
qibobo <qiyangbj@cn.ibm.com>
2019-01-29 15:50:21 +0800

    add prometheus health endpoint for service broker
    [#163488428]

---
+++ servicebroker/lib/healthendpoint/health.js
@@ -0,0 +1,16 @@
+'use strict';
+var client = require('prom-client');
+var register = client.register
+var collectDefaultMetrics = client.collectDefaultMetrics;
+collectDefaultMetrics({ prefix: 'autoscaler_servicebroker_' });
+module.exports = function () {
+  
+
+  var express = require('express');
+  var router = express.Router();
+  router.get('/', function (req, res) {
+    res.set('Content-Type', register.contentType);
+    res.send(register.metrics());
+  });
+  return router;
+}
