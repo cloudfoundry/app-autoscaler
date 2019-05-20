dfc06c6019c39c66963dad58cc991291070c6d32
qibobo <qiyangbj@cn.ibm.com>
2019-05-20 14:13:03 +0800

    make golang apiserver's public api the same as the nodejs apiserver(instance_metrics)

--- src/autoscaler/models/api.go
+++ src/autoscaler/models/api.go
@@ -21,3 +21,24 @@ type UnbindingRequestBody struct {
 	BrokerCommonRequestBody
 	AppID string `json:"app_guid"`
 }
+
+type PublicApiResultBase struct {
+	TotalResults int           `json:"total_results"`
+	TotalPages   int           `json:"total_pages"`
+	Page         int           `json:"page"`
+	PrevUrl      string        `json:"prev_url"`
+	NextUrl      string        `json:"next_url"`
+	Resources    []interface{} `json:"resources"`
+}
+type InstanceMetricResult struct {
+	PublicApiResultBase
+	Resources []AppInstanceMetric `json:"resources"`
+}
+type AppMetricResult struct {
+	PublicApiResultBase
+	Resources []AppMetric `json:"resources"`
+}
+type AppScalingHistoryResult struct {
+	PublicApiResultBase
+	Resources []AppScalingHistory `json:"resources"`
+}
