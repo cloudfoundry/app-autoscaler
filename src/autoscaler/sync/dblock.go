0119c044784316811272b0f84247acf2fec60012
qibobo <qiyangbj@cn.ibm.com>
2019-06-20 09:54:38 +0800

    when operator lost the dblock, it should exit

--- src/autoscaler/sync/dblock.go
+++ src/autoscaler/sync/dblock.go
@@ -3,7 +3,9 @@ package sync
 import (
 	"autoscaler/db"
 	"autoscaler/models"
+
 	"os"
+	"sync/atomic"
 	"time"
 
 	"code.cloudfoundry.org/lager"
@@ -11,23 +13,31 @@ import (
 	"github.com/tedsuo/ifrit"
 )
 
+const (
+	OwnLock = iota
+	LostLock
+)
+
 type DatabaseLock struct {
 	logger lager.Logger
+	// isHoldLock represents the lock status of the last acquiring.
+	isHoldLock int32
 }
 
 func NewDatabaseLock(logger lager.Logger) *DatabaseLock {
 	return &DatabaseLock{
-		logger: logger,
+		logger:     logger,
+		isHoldLock: LostLock,
 	}
 }
 
-func (dblock *DatabaseLock) InitDBLockRunner(retryInterval time.Duration, ttl time.Duration, owner string, lockDB db.LockDB) ifrit.Runner {
+func (dblock *DatabaseLock) InitDBLockRunner(retryInterval time.Duration, ttl time.Duration, owner string, lockDB db.LockDB, callbackOnAcquiredLock func(), callbackOnLostLock func()) ifrit.Runner {
 	dbLockMaintainer := ifrit.RunFunc(func(signals <-chan os.Signal, ready chan<- struct{}) error {
 		lockTicker := time.NewTicker(retryInterval)
 		readyToAcquireLock := true
 		if owner == "" {
 			dblock.logger.Info("failed-to-get-owner-details")
-			os.Exit(1)
+			callbackOnLostLock()
 		}
 		lock := &models.Lock{Owner: owner, Ttl: ttl}
 		isLockAcquired, lockErr := lockDB.Lock(lock)
@@ -36,7 +46,9 @@ func (dblock *DatabaseLock) InitDBLockRunner(retryInterval time.Duration, ttl ti
 		}
 		if isLockAcquired {
 			readyToAcquireLock = false
+			atomic.StoreInt32(&dblock.isHoldLock, OwnLock)
 			dblock.logger.Info("lock-acquired-in-first-attempt", lager.Data{"owner": owner, "isLockAcquired": isLockAcquired})
+			callbackOnAcquiredLock()
 			close(ready)
 		}
 		for {
@@ -48,6 +60,7 @@ func (dblock *DatabaseLock) InitDBLockRunner(retryInterval time.Duration, ttl ti
 				if releaseErr != nil {
 					dblock.logger.Error("failed-to-release-lock ", releaseErr)
 				} else {
+					atomic.StoreInt32(&dblock.isHoldLock, LostLock)
 					dblock.logger.Debug("successfully-released-lock", lager.Data{"owner": owner})
 				}
 				readyToAcquireLock = true
@@ -63,13 +76,24 @@ func (dblock *DatabaseLock) InitDBLockRunner(retryInterval time.Duration, ttl ti
 					if releaseErr != nil {
 						dblock.logger.Error("failed-to-release-lock ", releaseErr)
 					} else {
+						atomic.StoreInt32(&dblock.isHoldLock, LostLock)
 						dblock.logger.Info("successfully-released-lock", lager.Data{"owner": owner})
 					}
-					os.Exit(1)
+					callbackOnLostLock()
+				}
+				if !isLockAcquired {
+					previousLockStatus := atomic.LoadInt32(&dblock.isHoldLock)
+					if previousLockStatus == OwnLock {
+						dblock.logger.Info("lock-has-been-acquired-by-competitor")
+						atomic.StoreInt32(&dblock.isHoldLock, LostLock)
+						callbackOnLostLock()
+					}
 				}
 				if isLockAcquired && readyToAcquireLock {
 					readyToAcquireLock = false
 					dblock.logger.Info("successfully-acquired-lock", lager.Data{"owner": owner})
+					atomic.StoreInt32(&dblock.isHoldLock, OwnLock)
+					callbackOnAcquiredLock()
 					close(ready)
 				}
 			}
---
+++ src/autoscaler/sync/sync_suite_test.go
@@ -0,0 +1,91 @@
+package sync_test
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+
+	"autoscaler/db"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"testing"
+)
+
+func TestSync(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Sync Suite")
+}
+
+var (
+	dbHelper      *sql.DB
+	lockTableName = "test_lock"
+)
+var _ = BeforeSuite(func() {
+	var e error
+
+	dbUrl := os.Getenv("DBURL")
+	if dbUrl == "" {
+		Fail("environment variable $DBURL is not set")
+	}
+
+	dbHelper, e = sql.Open(db.PostgresDriverName, dbUrl)
+	if e != nil {
+		Fail("can not connect database: " + e.Error())
+	}
+
+	e = createLockTable()
+	if e != nil {
+		Fail("can not create test lock table: " + e.Error())
+	}
+
+})
+
+var _ = AfterSuite(func() {
+	if dbHelper != nil {
+		dbHelper.Close()
+	}
+
+})
+
+func getLockOwner() string {
+	var owner string
+	query := fmt.Sprintf("SELECT owner FROM %s", lockTableName)
+	row := dbHelper.QueryRow(query)
+	err := row.Scan(&owner)
+	if err == sql.ErrNoRows {
+		owner = ""
+	}
+	return owner
+}
+
+func cleanLockTable() error {
+	_, err := dbHelper.Exec(fmt.Sprintf("DELETE FROM %s", lockTableName))
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func dropLockTable() error {
+	_, err := dbHelper.Exec(fmt.Sprintf("DROP TABLE %s", lockTableName))
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func createLockTable() error {
+	_, err := dbHelper.Exec(fmt.Sprintf(`
+		CREATE TABLE IF NOT EXISTS %s (
+			owner VARCHAR(255) PRIMARY KEY,
+			lock_timestamp TIMESTAMP  NOT NULL,
+			ttl BIGINT DEFAULT 0
+		);
+	`, lockTableName))
+	if err != nil {
+		return err
+	}
+	return nil
+}
