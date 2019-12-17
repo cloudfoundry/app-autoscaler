994eab534be621a988946c13939f5aceafe19595
qibobo <qiyangbj@cn.ibm.com>
2019-12-17 14:15:25 +0800

    upgrade prometheus client_golang to v1.2.1

--- src/autoscaler/healthendpoint/registrar.go
+++ src/autoscaler/healthendpoint/registrar.go
@@ -10,7 +10,12 @@ import (
 func RegisterCollectors(registrar prometheus.Registerer, collectors []prometheus.Collector, includeDefault bool, logger lager.Logger) {
 
 	if includeDefault {
-		err := registrar.Register(prometheus.NewProcessCollector(os.Getpid(), ""))
+		err := registrar.Register(prometheus.NewProcessCollector(prometheus.ProcessCollectorOpts{
+			PidFn: func() (int, error) {
+				return os.Getpid(), nil
+			},
+			Namespace: "",
+		}))
 		if err != nil {
 			logger.Error("Failed to register process collector", err)
 		}
