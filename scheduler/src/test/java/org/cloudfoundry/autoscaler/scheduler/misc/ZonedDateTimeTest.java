5a438b1f0ef37f24ddaa573dfbb589968e4bd289
Silvestre Zabala <silvestre.zabala@sap.com>
2020-01-31 11:07:15 +0100

    Use Java 11 instead of 8
    As travis is currently failing with
    
    ```
    install-jdk.sh 2020-01-14
    
    Expected feature release number in range of 9 to 15, but got: 8
    ```

--- scheduler/src/test/java/org/cloudfoundry/autoscaler/scheduler/misc/ZonedDateTimeTest.java
+++ scheduler/src/test/java/org/cloudfoundry/autoscaler/scheduler/misc/ZonedDateTimeTest.java
@@ -5,8 +5,10 @@ import static org.junit.Assert.assertThat;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAccessor;
 import java.util.TimeZone;
 
 import org.cloudfoundry.autoscaler.scheduler.util.DateHelper;
@@ -53,51 +55,59 @@ public class ZonedDateTimeTest {
 		ZonedDateTime convertedDate = getZonedDateTimeForDateTime("2017-03-12T01:00",
 				TimeZone.getTimeZone(policyTimeZone));
 		assertThat("Zoned date time is as expected when it is not DST in Montreal", convertedDate,
-				is(ZonedDateTime.parse("2017-03-12T01:00Z[" + policyTimeZone + "]")));
+				is(parseZonedDateTime("2017-03-12T01:00Z[" + policyTimeZone + "]")));
 
 		convertedDate = getZonedDateTimeForDateTime("2017-03-12T02:00", TimeZone.getTimeZone(policyTimeZone));
 		assertThat("Zoned date time is as expected at the time of DST start in Montreal", convertedDate,
-				is(ZonedDateTime.parse("2017-03-12T02:00Z[" + policyTimeZone + "]")));
+				is(parseZonedDateTime("2017-03-12T02:00Z[" + policyTimeZone + "]")));
 
 		convertedDate = getZonedDateTimeForDateTime("2017-03-12T03:00", TimeZone.getTimeZone(policyTimeZone));
 		assertThat("Zoned date time is as expected when DST in Montreal", convertedDate,
-				is(ZonedDateTime.parse("2017-03-12T03:00Z[" + policyTimeZone + "]")));
+				is(parseZonedDateTime("2017-03-12T03:00Z[" + policyTimeZone + "]")));
 
 		convertedDate = getZonedDateTimeForDateTime("2017-11-05T01:00", TimeZone.getTimeZone(policyTimeZone));
 		assertThat("Zoned date time is as expected at the time of DST end in Montreal", convertedDate,
-				is(ZonedDateTime.parse("2017-11-05T01:00Z[" + policyTimeZone + "]")));
+				is(parseZonedDateTime("2017-11-05T01:00Z[" + policyTimeZone + "]")));
 
 		convertedDate = getZonedDateTimeForDateTime("2017-11-05T02:00", TimeZone.getTimeZone(policyTimeZone));
 		assertThat("Zoned date time is as expected just after DST ended in Montreal", convertedDate,
-				is(ZonedDateTime.parse("2017-11-05T02:00Z[" + policyTimeZone + "]")));
+				is(parseZonedDateTime("2017-11-05T02:00Z[" + policyTimeZone + "]")));
 
 		convertedDate = getZonedDateTimeForDateTime("2017-10-01T01:00", TimeZone.getTimeZone(policyTimeZone));
 		assertThat("Zoned date time is as expected when it is not DST in Sydney", convertedDate,
-				is(ZonedDateTime.parse("2017-10-01T01:00Z[" + policyTimeZone + "]")));
+				is(parseZonedDateTime("2017-10-01T01:00Z[" + policyTimeZone + "]")));
 
 		convertedDate = getZonedDateTimeForDateTime("2017-10-01T02:00", TimeZone.getTimeZone(policyTimeZone));
 		assertThat("Zoned date time is as expected at the time of DST start in Sydney", convertedDate,
-				is(ZonedDateTime.parse("2017-10-01T02:00Z[" + policyTimeZone + "]")));
+				is(parseZonedDateTime("2017-10-01T02:00Z[" + policyTimeZone + "]")));
 
 		convertedDate = getZonedDateTimeForDateTime("2017-10-01T03:00", TimeZone.getTimeZone(policyTimeZone));
 		assertThat("Zoned date time is as expected when DST in Sydney", convertedDate,
-				is(ZonedDateTime.parse("2017-10-01T03:00Z[" + policyTimeZone + "]")));
+				is(parseZonedDateTime("2017-10-01T03:00Z[" + policyTimeZone + "]")));
 
 		convertedDate = getZonedDateTimeForDateTime("2017-04-02T02:00", TimeZone.getTimeZone(policyTimeZone));
 		assertThat("Zoned date time is as expected at the time of DST end in Sydney", convertedDate,
-				is(ZonedDateTime.parse("2017-04-02T02:00Z[" + policyTimeZone + "]")));
+				is(parseZonedDateTime("2017-04-02T02:00Z[" + policyTimeZone + "]")));
 
 		convertedDate = getZonedDateTimeForDateTime("2017-04-02T03:00", TimeZone.getTimeZone(policyTimeZone));
 		assertThat("Zoned date time is as expected just after DST ended in Sydney", convertedDate,
-				is(ZonedDateTime.parse("2017-04-02T03:00Z[" + policyTimeZone + "]")));
+				is(parseZonedDateTime("2017-04-02T03:00Z[" + policyTimeZone + "]")));
 
 	}
 
 	private void checkDateHelper_getZonedDateTimeForDate(String policyTimeZone) {
 		// Test the date is correct for different system timezones and different policy timezone.
 		ZonedDateTime convertedDate = getZonedDateTimeForDate("2017-03-12", TimeZone.getTimeZone(policyTimeZone));
-		assertThat(convertedDate, is(ZonedDateTime.parse("2017-03-12T00:00Z[" + policyTimeZone + "]")));
+		assertThat(convertedDate, is(parseZonedDateTime("2017-03-12T00:00Z[" + policyTimeZone + "]")));
 
+	}
+
+	private ZonedDateTime parseZonedDateTime(String policyTimeZone) {
+		// https://stackoverflow.com/questions/56255020/zoneddatetime-change-behavior-jdk-8-11
+	    TemporalAccessor parsed = DateTimeFormatter.ISO_ZONED_DATE_TIME.parse(policyTimeZone);
+	    LocalDateTime dateTime = LocalDateTime.from(parsed);
+	    ZoneId zone = ZoneId.from(parsed);
+	    return dateTime.atZone(zone);
 	}
 
 	private ZonedDateTime getZonedDateTimeForDateTime(String inputDateTimeStr, TimeZone timeZone) {
