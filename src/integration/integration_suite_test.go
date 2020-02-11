cloudfoundry/app-autoscaler

commit 2cfa708e2b5e660692647de5aeb84bbede0c368a

Also check default policy against plan in update

src/autoscaler/api/brokerserver/broker_handler.go
@@ -38,6 +38,7 @@ type BrokerHandler struct {
 	quotaManagementClient *quota.QuotaManagementClient
 	catalog               []domain.Service
 	planChecker           *plancheck.PlanChecker
+	cfClient              cf.CFClient
 }
 
 var emptyJSONObject = regexp.MustCompile(`^\s*{\s*}\s*$`)
@@ -55,6 +56,7 @@ func NewBrokerHandler(logger lager.Logger, conf *config.Config, bindingdb db.Bin
 		schedulerUtil:         schedulerutil.NewSchedulerUtil(conf, logger),
 		quotaManagementClient: quota.NewQuotaManagementClient(conf.QuotaManagement, logger, cfClient),
 		planChecker:           plancheck.NewPlanChecker(conf.PlanCheck, logger),
+		cfClient:              cfClient,
 	}
 
 }
@@ -288,6 +290,16 @@ func (h *BrokerHandler) UpdateServiceInstance(w http.ResponseWriter, r *http.Req
 		if !valid {
 			h.logger.Error("failed to validate policy", err, lager.Data{"instanceId": instanceId, "policy": updatedDefaultPolicy})
 			handlers.WriteJSONResponse(w, http.StatusBadRequest, errResults)
+			return
+		}
+
+		servicePlan, err := h.cfClient.GetServicePlan(instanceId)
+		if err != nil {
+			h.logger.Error("failed-to-retrieve-service-plan-of-service-instance", err, lager.Data{"instanceId": instanceId})
+			writeErrorResponse(w, http.StatusInternalServerError, "Error validating policy")
+			return
+		}
+		if h.planDefinitionExceeded(updatedDefaultPolicy, servicePlan, instanceId, w) {
 			return
 		}
 

src/autoscaler/api/brokerserver/broker_handler_test.go
@@ -27,9 +27,10 @@ import (
 
 var _ = Describe("BrokerHandler", func() {
 	var (
-		bindingdb *fakes.FakeBindingDB
-		policydb  *fakes.FakePolicyDB
-		sbssdb    *fakes.FakeSbssDB
+		bindingdb    *fakes.FakeBindingDB
+		policydb     *fakes.FakePolicyDB
+		sbssdb       *fakes.FakeSbssDB
+		fakecfClient *fakes.FakeCFClient
 
 		handler *BrokerHandler
 		resp    *httptest.ResponseRecorder
@@ -40,6 +41,7 @@ var _ = Describe("BrokerHandler", func() {
 		policydb = &fakes.FakePolicyDB{}
 		sbssdb = &fakes.FakeSbssDB{}
 		resp = httptest.NewRecorder()
+		fakecfClient = &fakes.FakeCFClient{}
 	})
 
 	JustBeforeEach(func() {
@@ -50,7 +52,7 @@ var _ = Describe("BrokerHandler", func() {
 				ID:   "a-plan-id",
 				Name: "standard",
 			}},
-		}}, nil)
+		}}, fakecfClient)
 	})
 
 	Describe("GetBrokerCatalog", func() {
@@ -509,6 +511,7 @@ var _ = Describe("BrokerHandler", func() {
 				bindingdb.GetAppIdsByInstanceIdReturns([]string{"app-id-1", "app-id-2"}, nil)
 				policydb.SetOrUpdateDefaultAppPolicyReturns([]string{"app-id-2"}, nil)
 				verifyScheduleIsUpdatedInScheduler("app-id-2", testDefaultPolicy)
+				fakecfClient.GetServicePlanReturns("a-plan-id", nil)
 			})
 			It("succeeds with 200, saves the default policy, and sets the default policy on the already bound apps", func() {
 				By("returning 200")
@@ -558,6 +561,7 @@ var _ = Describe("BrokerHandler", func() {
 				bindingdb.GetAppIdsByInstanceIdReturns([]string{"app-id-1", "app-id-2"}, nil)
 				policydb.SetOrUpdateDefaultAppPolicyReturns([]string{"app-id-2"}, nil)
 				verifyScheduleIsUpdatedInScheduler("app-id-2", testDefaultPolicy)
+				fakecfClient.GetServicePlanReturns("a-plan-id", nil)
 			})
 			It("succeeds with 200, saves the default policy, and updates the default policy", func() {
 				By("returning 200")

src/autoscaler/api/plancheck/plan_checker.go
@@ -27,7 +27,7 @@ func (pc PlanChecker) CheckPlan(policy models.ScalingPolicy, planID string) (boo
 	}
 	definition, ok := pc.conf.PlanDefinitions[planID]
 	if !ok {
-		return false, "", fmt.Errorf(`unknown plan id "%s".`, planID)
+		return false, "", fmt.Errorf(`unknown plan id "%s"`, planID)
 	}
 	if !definition.PlanCheckEnabled {
 		return true, "", nil

src/autoscaler/cf/client.go
@@ -57,23 +57,26 @@ type CFClient interface {
 	IsUserSpaceDeveloper(userToken string, appId string) (bool, error)
 	IsTokenAuthorized(token, clientId string) (bool, error)
 	GetServiceInstancesInOrg(orgGUID, servicePlanGuid string) (int, error)
+	GetServicePlan(serviceInstanceGuid string) (string, error)
 }
 
 type cfClient struct {
-	logger             lager.Logger
-	conf               *CFConfig
-	clk                clock.Clock
-	tokens             Tokens
-	endpoints          Endpoints
-	infoURL            string
-	tokenURL           string
-	introspectTokenURL string
-	loginForm          url.Values
-	authHeader         string
-	httpClient         *http.Client
-	lock               *sync.Mutex
-	grantTime          time.Time
-	servicePlanGuids   map[string]string
+	logger                              lager.Logger
+	conf                                *CFConfig
+	clk                                 clock.Clock
+	tokens                              Tokens
+	endpoints                           Endpoints
+	infoURL                             string
+	tokenURL                            string
+	introspectTokenURL                  string
+	loginForm                           url.Values
+	authHeader                          string
+	httpClient                          *http.Client
+	lock                                *sync.Mutex
+	grantTime                           time.Time
+	brokerPlanGuidToCCServicePlanGuid   map[string]string
+	ccServicePlanToBrokerPlanGuid       map[string]string
+	serviceInstanceGuidToBrokerPlanGuid map[string]string
 }
 
 func NewCFClient(conf *CFConfig, logger lager.Logger, clk clock.Clock) CFClient {
@@ -98,7 +101,9 @@ func NewCFClient(conf *CFConfig, logger lager.Logger, clk clock.Clock) CFClient 
 
 	c.lock = &sync.Mutex{}
 
-	c.servicePlanGuids = make(map[string]string)
+	c.brokerPlanGuidToCCServicePlanGuid = make(map[string]string)
+	c.ccServicePlanToBrokerPlanGuid = make(map[string]string)
+	c.serviceInstanceGuidToBrokerPlanGuid = make(map[string]string)
 
 	return c
 }

src/autoscaler/cf/service_instances.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net/http"
 	"net/url"
+	"path"
 
 	"code.cloudfoundry.org/lager"
 )
@@ -15,8 +16,8 @@ const (
 	ResultsPerPageParam  = "results-per-page"
 )
 
-func (c *cfClient) GetServiceInstancesInOrg(orgGUID, servicePlanGuid string) (int, error) {
-	resolvedGuid, err := c.getServicePlanGuid(servicePlanGuid)
+func (c *cfClient) GetServiceInstancesInOrg(orgGUID, brokerServicePlanGuid string) (int, error) {
+	ccServicePlanGuid, err := c.getCCServicePlanGuid(brokerServicePlanGuid)
 	if err != nil {
 		return 0, fmt.Errorf("cf-client-get-service-instances-in-org: failed to resolve service plan guid: %w", err)
 	}
@@ -29,7 +30,7 @@ func (c *cfClient) GetServiceInstancesInOrg(orgGUID, servicePlanGuid string) (in
 
 	parameters := url.Values{}
 	parameters.Add("q", "organization_guid:"+orgGUID)
-	parameters.Add("q", "service_plan_guid:"+resolvedGuid)
+	parameters.Add("q", "service_plan_guid:"+ccServicePlanGuid)
 	parameters.Add(ResultsPerPageParam, "1")
 	servicesUrl.RawQuery = parameters.Encode()
 
@@ -81,12 +82,28 @@ type Result struct {
 	Resources    []Resource `json:"resources"`
 }
 
-func (c *cfClient) getServicePlanGuid(servicePlanGuid string) (string, error) {
-	logger := c.logger.Session("cf-client-get-service-plan-guid", lager.Data{"servicePlanGuid": servicePlanGuid})
+type ServiceInstanceEntity struct {
+	ServicePlanGuid string `json:"service_plan_guid"`
+}
+
+type ServiceInstanceResource struct {
+	Entity ServiceInstanceEntity `json:"entity"`
+}
+
+type ServicePlanEntity struct {
+	UniqueId string `json:"unique_id"`
+}
+
+type ServicePlanResource struct {
+	Entity ServicePlanEntity `json:"entity"`
+}
+
+func (c *cfClient) getCCServicePlanGuid(brokerPlanGuid string) (string, error) {
+	logger := c.logger.Session("cf-client-get-service-plan-guid", lager.Data{"brokerPlanGuid": brokerPlanGuid})
 	logger.Debug("start")
 	defer logger.Debug("end")
 
-	if g, ok := c.servicePlanGuids[servicePlanGuid]; ok {
+	if g, ok := c.brokerPlanGuidToCCServicePlanGuid[brokerPlanGuid]; ok {
 		return g, nil
 	}
 
@@ -97,7 +114,7 @@ func (c *cfClient) getServicePlanGuid(servicePlanGuid string) (string, error) {
 	servicePlansUrl.Path = servicePlansUrl.Path + ServicePlansPath
 
 	parameters := url.Values{}
-	parameters.Add("q", "unique_id:"+servicePlanGuid)
+	parameters.Add("q", "unique_id:"+brokerPlanGuid)
 	servicePlansUrl.RawQuery = parameters.Encode()
 
 	logger.Info("created-url", lager.Data{"url": servicePlansUrl.String()})
@@ -138,9 +155,125 @@ func (c *cfClient) getServicePlanGuid(servicePlanGuid string) (string, error) {
 		return "", err
 	}
 
-	resolvedGuid := result.Resources[0].Metadata.Guid
-	logger.Info("found-guid", lager.Data{"resolvedGuid": resolvedGuid})
-	c.servicePlanGuids[servicePlanGuid] = resolvedGuid
+	ccServicePlanGuid := result.Resources[0].Metadata.Guid
+	logger.Info("found-guid", lager.Data{"ccServicePlanGuid": ccServicePlanGuid})
+	c.brokerPlanGuidToCCServicePlanGuid[brokerPlanGuid] = ccServicePlanGuid
+	c.ccServicePlanToBrokerPlanGuid[ccServicePlanGuid] = brokerPlanGuid
 
-	return c.servicePlanGuids[servicePlanGuid], nil
+	return ccServicePlanGuid, nil
+}
+
+func (c *cfClient) getBrokerPlanGuid(ccServicePlanGuid string) (string, error) {
+	logger := c.logger.Session("cf-client-get-broker-plan-guid", lager.Data{"ccServicePlanGuid": ccServicePlanGuid})
+	logger.Debug("start")
+	defer logger.Debug("end")
+
+	if g, ok := c.ccServicePlanToBrokerPlanGuid[ccServicePlanGuid]; ok {
+		return g, nil
+	}
+
+	servicePlansUrl, err := url.Parse(c.conf.API)
+	if err != nil {
+		return "", fmt.Errorf("cf-client-get-broker-plan-guid: failed to parse CF API URL: %w", err)
+	}
+	servicePlansUrl.Path = servicePlansUrl.Path + path.Join(ServicePlansPath, ccServicePlanGuid)
+
+	logger.Info("created-url", lager.Data{"url": servicePlansUrl.String()})
+
+	req, err := http.NewRequest("GET", servicePlansUrl.String(), nil)
+	if err != nil {
+		logger.Error("new-request", err)
+		return "", fmt.Errorf("cf-client-get-broker-plan-guid: failed to create request to CF API: %w", err)
+	}
+	req.Header.Set("Authorization", TokenTypeBearer+" "+c.GetTokens().AccessToken)
+
+	var resp *http.Response
+	resp, err = c.httpClient.Do(req)
+
+	if err != nil {
+		logger.Error("do-request", err)
+		return "", fmt.Errorf("cf-client-get-broker-plan-guid: failed to execute request to CF API: %w", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		err = fmt.Errorf("cf-client-get-broker-plan-guid: failed to get service plan: %s [%d] %s", servicePlansUrl.String(), resp.StatusCode, resp.Status)
+		logger.Error("get-response", err)
+		return "", err
+	}
+
+	result := &ServicePlanResource{}
+
+	err = json.NewDecoder(resp.Body).Decode(result)
+	if err != nil {
+		logger.Error("decode", err)
+		return "", fmt.Errorf("cf-client-get-broker-plan-guid: failed to decode response from CF API: %w", err)
+	}
+
+	brokerPlanGuid := result.Entity.UniqueId
+	logger.Info("found-guid", lager.Data{"brokerPlanGuid": brokerPlanGuid})
+	c.ccServicePlanToBrokerPlanGuid[ccServicePlanGuid] = brokerPlanGuid
+	c.brokerPlanGuidToCCServicePlanGuid[brokerPlanGuid] = ccServicePlanGuid
+
+	return brokerPlanGuid, nil
+}
+
+func (c *cfClient) GetServicePlan(serviceInstanceGuid string) (string, error) {
+	logger := c.logger.Session("cf-client-get-service-plan", lager.Data{"serviceInstanceGuid": serviceInstanceGuid})
+	logger.Debug("start")
+	defer logger.Debug("end")
+
+	if g, ok := c.serviceInstanceGuidToBrokerPlanGuid[serviceInstanceGuid]; ok {
+		return g, nil
+	}
+
+	serviceInstancesUrl, err := url.Parse(c.conf.API)
+	if err != nil {
+		return "", fmt.Errorf("cf-client-get-service-plan: failed to parse CF API URL: %w", err)
+	}
+	serviceInstancesUrl.Path = path.Join(ServiceInstancesPath, serviceInstanceGuid)
+
+	logger.Debug("created-url", lager.Data{"url": serviceInstancesUrl.String()})
+
+	req, err := http.NewRequest("GET", serviceInstancesUrl.String(), nil)
+	if err != nil {
+		logger.Error("new-request", err)
+		return "", fmt.Errorf("cf-client-get-service-plan: failed to create request to CF API: %w", err)
+	}
+	req.Header.Set("Authorization", TokenTypeBearer+" "+c.GetTokens().AccessToken)
+
+	var resp *http.Response
+	resp, err = c.httpClient.Do(req)
+
+	if err != nil {
+		logger.Error("do-request", err)
+		return "", fmt.Errorf("cf-client-get-service-plan: failed to execute request to CF API: %w", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		err = fmt.Errorf("cf-client-get-service-plan: failed to get service plan: %s [%d] %s", serviceInstancesUrl.String(), resp.StatusCode, resp.Status)
+		logger.Error("get-response", err)
+		return "", err
+	}
+
+	result := &ServiceInstanceResource{}
+
+	err = json.NewDecoder(resp.Body).Decode(result)
+	if err != nil {
+		logger.Error("decode", err)
+		return "", fmt.Errorf("cf-client-get-service-plan: failed to decode response from CF API: %w", err)
+	}
+
+	servicePlanGuid := result.Entity.ServicePlanGuid
+	logger.Info("found-guid", lager.Data{"servicePlanGuid": servicePlanGuid})
+	brokerPlanGuid, err := c.getBrokerPlanGuid(servicePlanGuid)
+	if err != nil {
+		logger.Error("cc-plan-to-broker-plan", err)
+		return "", fmt.Errorf("cf-client-get-service-plan: failed to translate Cloud Controller service plan to broker service plan: %w", err)
+	}
+
+	c.serviceInstanceGuidToBrokerPlanGuid[serviceInstanceGuid] = brokerPlanGuid
+
+	return c.serviceInstanceGuidToBrokerPlanGuid[serviceInstanceGuid], nil
 }

src/autoscaler/fakes/fake_cf_client.go
@@ -45,6 +45,19 @@ type FakeCFClient struct {
 		result1 int
 		result2 error
 	}
+	GetServicePlanStub        func(string) (string, error)
+	getServicePlanMutex       sync.RWMutex
+	getServicePlanArgsForCall []struct {
+		arg1 string
+	}
+	getServicePlanReturns struct {
+		result1 string
+		result2 error
+	}
+	getServicePlanReturnsOnCall map[int]struct {
+		result1 string
+		result2 error
+	}
 	GetTokensStub        func() cf.Tokens
 	getTokensMutex       sync.RWMutex
 	getTokensArgsForCall []struct {
@@ -309,6 +322,69 @@ func (fake *FakeCFClient) GetServiceInstancesInOrgReturnsOnCall(i int, result1 i
 	}
 	fake.getServiceInstancesInOrgReturnsOnCall[i] = struct {
 		result1 int
+		result2 error
+	}{result1, result2}
+}
+
+func (fake *FakeCFClient) GetServicePlan(arg1 string) (string, error) {
+	fake.getServicePlanMutex.Lock()
+	ret, specificReturn := fake.getServicePlanReturnsOnCall[len(fake.getServicePlanArgsForCall)]
+	fake.getServicePlanArgsForCall = append(fake.getServicePlanArgsForCall, struct {
+		arg1 string
+	}{arg1})
+	fake.recordInvocation("GetServicePlan", []interface{}{arg1})
+	fake.getServicePlanMutex.Unlock()
+	if fake.GetServicePlanStub != nil {
+		return fake.GetServicePlanStub(arg1)
+	}
+	if specificReturn {
+		return ret.result1, ret.result2
+	}
+	fakeReturns := fake.getServicePlanReturns
+	return fakeReturns.result1, fakeReturns.result2
+}
+
+func (fake *FakeCFClient) GetServicePlanCallCount() int {
+	fake.getServicePlanMutex.RLock()
+	defer fake.getServicePlanMutex.RUnlock()
+	return len(fake.getServicePlanArgsForCall)
+}
+
+func (fake *FakeCFClient) GetServicePlanCalls(stub func(string) (string, error)) {
+	fake.getServicePlanMutex.Lock()
+	defer fake.getServicePlanMutex.Unlock()
+	fake.GetServicePlanStub = stub
+}
+
+func (fake *FakeCFClient) GetServicePlanArgsForCall(i int) string {
+	fake.getServicePlanMutex.RLock()
+	defer fake.getServicePlanMutex.RUnlock()
+	argsForCall := fake.getServicePlanArgsForCall[i]
+	return argsForCall.arg1
+}
+
+func (fake *FakeCFClient) GetServicePlanReturns(result1 string, result2 error) {
+	fake.getServicePlanMutex.Lock()
+	defer fake.getServicePlanMutex.Unlock()
+	fake.GetServicePlanStub = nil
+	fake.getServicePlanReturns = struct {
+		result1 string
+		result2 error
+	}{result1, result2}
+}
+
+func (fake *FakeCFClient) GetServicePlanReturnsOnCall(i int, result1 string, result2 error) {
+	fake.getServicePlanMutex.Lock()
+	defer fake.getServicePlanMutex.Unlock()
+	fake.GetServicePlanStub = nil
+	if fake.getServicePlanReturnsOnCall == nil {
+		fake.getServicePlanReturnsOnCall = make(map[int]struct {
+			result1 string
+			result2 error
+		})
+	}
+	fake.getServicePlanReturnsOnCall[i] = struct {
+		result1 string
 		result2 error
 	}{result1, result2}
 }
@@ -733,6 +809,8 @@ func (fake *FakeCFClient) Invocations() map[string][][]interface{} {
 	defer fake.getEndpointsMutex.RUnlock()
 	fake.getServiceInstancesInOrgMutex.RLock()
 	defer fake.getServiceInstancesInOrgMutex.RUnlock()
+	fake.getServicePlanMutex.RLock()
+	defer fake.getServicePlanMutex.RUnlock()
 	fake.getTokensMutex.RLock()
 	defer fake.getTokensMutex.RUnlock()
 	fake.isTokenAuthorizedMutex.RLock()

commit cab58dccd3030aac89eba7e32713854ad05c5ab9

Integration tests: Fix for policy check in update
Fix for breakage introduced in 2cfa708e2b5e660692647de5aeb84bbede0c368a

src/integration/integration_suite_test.go
@@ -67,6 +67,8 @@ var (
 	appSummaryRegPath       = regexp.MustCompile(`^/v2/apps/.*/summary$`)
 	appInstanceRegPath      = regexp.MustCompile(`^/v2/apps/.*$`)
 	checkUserSpaceRegPath   = regexp.MustCompile(`^/v2/users/.+/spaces.*$`)
+	serviceInstanceRegPath  = regexp.MustCompile(`^/v2/service_instances/.*$`)
+	servicePlanRegPath      = regexp.MustCompile(`^/v2/service_plans/.*$`)
 	dbHelper                *sql.DB
 	fakeCCNOAAUAA           *ghttp.Server
 	messagesToSend          chan []byte
@@ -781,6 +783,26 @@ func startFakeCCNOAAUAA(instanceCount int) {
 			TotalResults int `json:"total_results"`
 		}{
 			1,
+		}))
+
+	type ServiceInstanceEntity struct {
+		ServicePlanGuid string `json:"service_plan_guid"`
+	}
+	fakeCCNOAAUAA.RouteToHandler("GET", serviceInstanceRegPath, ghttp.RespondWithJSONEncoded(http.StatusOK,
+		struct {
+			ServiceInstanceEntity `json:"entity"`
+		}{
+			ServiceInstanceEntity{"cc-free-plan-id"},
+		}))
+
+	type ServicePlanEntity struct {
+		UniqueId string `json:"unique_id"`
+	}
+	fakeCCNOAAUAA.RouteToHandler("GET", servicePlanRegPath, ghttp.RespondWithJSONEncoded(http.StatusOK,
+		struct {
+			ServicePlanEntity `json:"entity"`
+		}{
+			ServicePlanEntity{"autoscaler-free-plan-id"},
 		}))
 }
 func fakeMetricsPolling(appId string, memoryValue uint64, memQuota uint64) {