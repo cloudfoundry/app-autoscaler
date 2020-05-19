3d47083dff4a11bcfb1fde41a1002c9275ed1475
Aadesh Misra <aadesh.misra@sap.com>
2020-05-19 11:12:21 +0200

    component names removed

--- src/autoscaler/models/health.go
+++ src/autoscaler/models/health.go
@@ -2,7 +2,6 @@ package models
 
 import (
 	"fmt"
-
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -14,34 +13,34 @@ type HealthConfig struct {
 	HealthCheckPasswordHash string `yaml:"password_hash"`
 }
 
-func (c *HealthConfig) Validate(component string) error {
+func (c *HealthConfig) Validate() error {
 
 	if c.HealthCheckUsername != "" && c.HealthCheckUsernameHash != "" {
-		return fmt.Errorf("Configuration error: both %s healthcheck username and %s healthcheck username_hash are set, please provide only one of them", component, component)
+		return fmt.Errorf("Configuration error: both healthcheck username and healthcheck username_hash are set, please provide only one of them")
 	}
 
 	if c.HealthCheckPassword != "" && c.HealthCheckPasswordHash != "" {
-		return fmt.Errorf("Configuration error: both %s healthcheck password and %s healthcheck password_hash are empty, please provide one of them", component, component)
+		return fmt.Errorf("Configuration error: both healthcheck password and healthcheck password_hash are empty, please provide one of them")
 	}
 
 	if c.HealthCheckUsernameHash != "" {
 		if _, err := bcrypt.Cost([]byte(c.HealthCheckUsernameHash)); err != nil {
-			return fmt.Errorf("Configuration error: %s healthcheck username_hash is not a valid bcrypt hash", component)
+			return fmt.Errorf("Configuration error: healthcheck username_hash is not a valid bcrypt hash")
 		}
 	}
 
 	if c.HealthCheckPasswordHash != "" {
 		if _, err := bcrypt.Cost([]byte(c.HealthCheckPasswordHash)); err != nil {
-			return fmt.Errorf("Configuration error: %s healthcheck password_hash is not a valid bcrypt hash", component)
+			return fmt.Errorf("Configuration error: healthcheck password_hash is not a valid bcrypt hash")
 		}
 	}
 
 	if c.HealthCheckUsername == "" && c.HealthCheckPassword != "" {
-		return fmt.Errorf("Configuration error: %s healthcheck username is empty", component)
+		return fmt.Errorf("Configuration error: healthcheck username is empty")
 	}
 
 	if c.HealthCheckUsername != "" && c.HealthCheckPassword == "" {
-		return fmt.Errorf("Configuration error: %s healthcheck password is empty", component)
+		return fmt.Errorf("Configuration error: healthcheck password is empty")
 	}
 
 	return nil
