0a18257380a70153dbcefc6d62b47317221adfab
Gareth Evans <g.evans@sap.com>
2021-07-02 10:54:25 +0100

    tools.go imports the correct package to fix vendoring

--- tools.go
+++ tools.go
@@ -6,5 +6,5 @@ import (
 	_ "github.com/onsi/ginkgo/ginkgo"
 	_ "github.com/square/certstrap"
 	_ "github.com/maxbrunsfeld/counterfeiter/v6"
-	_ "github.com/golangci/golangci-lint"
+	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
 )
