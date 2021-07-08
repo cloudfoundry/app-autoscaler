3619bf5c11141f4530692c1f7a0ce5449caa0a33
Gareth Evans <g.evans@sap.com>
2021-07-08 14:52:38 +0100

    Covert parsing to golang

---
+++ format_checkstyle.go
@@ -0,0 +1,53 @@
+package main
+
+
+import (
+    "fmt"
+    "io/ioutil"
+    "encoding/xml"
+    "os"
+)
+
+type Result struct {
+    Files   []File   `xml:"file"`
+}
+
+type File struct {
+    Name    string   `xml:"name,attr"`
+    Errors  []Error  `xml:"error"`
+}
+
+type Error struct {
+    Line     string   `xml:"line,attr"`
+    Column   string   `xml:"column,attr"`
+    Severity string   `xml:"severity,attr"`
+    Message  string   `xml:"message,attr"`
+    Source   string   `xml:"source,attr"`
+}
+
+func main() {
+    xmlFile, err := os.Open("scheduler/target/checkstyle-result.xml")
+    if err != nil {
+        panic(err)
+    }
+
+    fmt.Println("Successfully Opened file")
+    defer xmlFile.Close()
+
+    byteValue, err := ioutil.ReadAll(xmlFile)
+    if err != nil {
+        panic(err)
+    }
+
+    var result Result
+    err = xml.Unmarshal(byteValue, &result)
+    if err != nil {
+        panic(err)
+    }
+
+    for _, f := range result.Files {
+      for _, e := range f.Errors {
+        fmt.Printf("::%s file=%s,line=%s,col=%s::%s\n", e.Severity, f.Name, e.Line, e.Column, e.Message)
+      }
+    }
+}
