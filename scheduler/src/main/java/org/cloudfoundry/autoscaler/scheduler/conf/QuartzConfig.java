feb83f5a63f4cdab8923763ea4a30ebe21fa13dd
Arsalan Khan <muhammad.arsalan.khan@sap.com>
2021-07-09 12:35:29 +0200

    remove unused code

--- scheduler/src/main/java/org/cloudfoundry/autoscaler/scheduler/conf/QuartzConfig.java
+++ scheduler/src/main/java/org/cloudfoundry/autoscaler/scheduler/conf/QuartzConfig.java
@@ -1,18 +1,12 @@
 package org.cloudfoundry.autoscaler.scheduler.conf;
 
 import org.cloudfoundry.autoscaler.scheduler.quartz.QuartzJobFactory;
-import org.quartz.spi.TriggerFiredBundle;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
 import org.springframework.boot.autoconfigure.quartz.QuartzProperties;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.scheduling.quartz.SchedulerFactoryBean;
-import org.springframework.scheduling.quartz.SpringBeanJobFactory;
 import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
@@ -21,17 +15,11 @@ import java.util.Properties;
 @Configuration
 public class QuartzConfig {
 
-
-   /* @Value("${spring.org.quartz.scheduler.instanceId}")
-    private String schedulerInstanceId;*/
-
-    private final DataSource primaryDataSource;
     private final PlatformTransactionManager primaryTransactionManager;
     private final QuartzProperties quartzProperties;
     private final ApplicationContext applicationContext;
 
-    public QuartzConfig(DataSource primaryDataSource, PlatformTransactionManager transactionManager, ApplicationContext applicationContext, QuartzProperties quartzProperties) {
-        this.primaryDataSource = primaryDataSource;
+    public QuartzConfig(PlatformTransactionManager transactionManager, ApplicationContext applicationContext, QuartzProperties quartzProperties) {
         this.primaryTransactionManager = transactionManager;
         this.applicationContext = applicationContext;
         this.quartzProperties = quartzProperties;
@@ -46,9 +34,7 @@ public class QuartzConfig {
         schedulerFactoryBean.setDataSource(primaryDataSource);
         schedulerFactoryBean.setTransactionManager(primaryTransactionManager);
         schedulerFactoryBean.setJobFactory(quartzJobFactory());
-
         schedulerFactoryBean.setQuartzProperties(getQuartzProperties());
-        //schedulerFactoryBean.setSchedulerName(schedulerName);
         schedulerFactoryBean.afterPropertiesSet();
         return schedulerFactoryBean;
     }
