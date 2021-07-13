5b6aedae5a039fca5ef6b0b46897ee53cc720198
Arsalan Khan <muhammad.arsalan.khan@sap.com>
2021-07-13 18:40:50 +0200

    add pre-commit config-> java-format hook
    add pre-commit config

---
+++ style-guide/format-java.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+###################################################################################################
+ # This pre-commit hook displays Java formatting issues
+###################################################################################################
+set -e -o pipefail
+
+REPO_PATH=$(git rev-parse --show-toplevel)
+DOWNLOAD_PATH="$REPO_PATH"/.cache
+echo "$DOWNLOAD_PATH"
+function download_google_formatter {
+    cd "$DOWNLOAD_PATH"
+    if [ ! -f "$DOWNLOAD_PATH"/google-java-format-1.10.0-all-deps.jar ] ; then
+        curl -LJO "https://github.com/google/google-java-format/releases/download/v1.10.0/google-java-format-1.10.0-all-deps.jar"
+        chmod 755 google-java-format-1.10.0-all-deps.jar
+    fi
+}
+changed_java_files=$(git diff --cached --name-only --diff-filter=ACMR | grep ".*java$" )
+if  [[ -n "$changed_java_files" ]]; then
+    echo "$DOWNLOAD_PATH"
+    mkdir -p "$DOWNLOAD_PATH"
+    download_google_formatter
+    cd "$REPO_PATH"
+fi
+
+# dry run Google formatter
+echo "download path : $DOWNLOAD_PATH"/google-java-format-1.10.0-all-deps.jar
+files_to_be_changed=$( java -jar "$DOWNLOAD_PATH"/google-java-format-1.10.0-all-deps.jar -n $changed_java_files)
+
+# if there are any stage changes
+if  [[ -n "$files_to_be_changed" ]]; then
+    echo "Formatter Results:"
+    echo "Analyzing java file(s) using google-java-format-1.10.0-all-deps.jar..."
+    echo "Incorrect formatting found:"
+    echo "$files_to_be_changed"
+    echo "Please correct the formatting of the files(s) using one of the following options:"
+    echo "  mvn fmt:format"
+    echo "  Reformat Code - IntelliJ or Format Document - Eclipse"
+    exit 2
+fi
