4588744e644847e0f1c972da07acbf39f2f4692a
Arsalan Khan <muhammad.arsalan.khan@sap.com>
2021-08-03 12:50:32 +0200

    aas82 add checkstyle configs

--- style-guide/inspect-java-format-0.1.sh
+++ style-guide/inspect-java-format-0.1.sh
@@ -6,8 +6,19 @@
 set -e -o pipefail
 
 REPO_PATH=$(git rev-parse --show-toplevel)
+CHECKSTYLE_CONFIG="google_checks.xml"
+CHECKSTYLE_CONFIG_PATH="$REPO_PATH"/style-guide/"$CHECKSTYLE_CONFIG"
 DOWNLOAD_PATH="$REPO_PATH"/.cache
-echo "$DOWNLOAD_PATH"
+CHECKSTYLE_JAR_NAME="checkstyle-8.44-all.jar"
+JAVA_SOURCES_PATH="$REPO_PATH"/scheduler/src/
+echo "Current Configs..."
+echo "REPO_PATH: $REPO_PATH"
+echo "DOWNLOAD_PATH: $DOWNLOAD_PATH"
+echo "JAVA_SOURCES_PATH: $JAVA_SOURCES_PATH"
+echo "CHECKSTYLE_JAR_NAME: $CHECKSTYLE_JAR_NAME"
+echo "CHECKSTYLE_CONFIG: $CHECKSTYLE_CONFIG"
+echo "CHECKSTYLE_CONFIG_PATH: $CHECKSTYLE_CONFIG_PATH"
+
 function download_google_formatter {
     cd "$DOWNLOAD_PATH"
     if [ ! -f "$DOWNLOAD_PATH"/google-java-format-1.10.0-all-deps.jar ] ; then
@@ -15,13 +26,36 @@ function download_google_formatter {
         chmod 755 google-java-format-1.10.0-all-deps.jar
     fi
 }
-changed_java_files=$(git diff --cached --name-only --diff-filter=ACMR | grep ".*java$" )
+
+function download_checkstyle {
+    cd "$DOWNLOAD_PATH"
+    echo "I am download_checkstyle"
+    if [ ! -f "$DOWNLOAD_PATH"/"$CHECKSTYLE_JAR_NAME" ] ; then
+        curl -LJO "https://github.com/checkstyle/checkstyle/releases/download/checkstyle-8.44/checkstyle-8.44-all.jar"
+        chmod 755 "$CHECKSTYLE_JAR_NAME"
+    fi
+}
+
+
+
 if  [[ -n "$changed_java_files" ]]; then
-    echo "$DOWNLOAD_PATH"
     mkdir -p "$DOWNLOAD_PATH"
     download_google_formatter
+    download_checkstyle
     cd "$REPO_PATH"
+
 fi
+
+download_checkstyle
+changed_java_files=$(git diff --cached --name-only --diff-filter=ACMR | grep ".*java$" )
+echo changed_java_files
+echo "...."
+
+    #checkstyle
+    echo "executing java -jar "$DOWNLOAD_PATH"/"$CHECKSTYLE_JAR_NAME" -c "$CHECKSTYLE_CONFIG_PATH" $changed_java_files"
+    checkstyle_result=$(java -jar "$DOWNLOAD_PATH"/"$CHECKSTYLE_JAR_NAME" -c "$CHECKSTYLE_CONFIG_PATH" $JAVA_SOURCES_PATH )
+    echo "exiting"
+    echo "results: $checkstyle_result"
 
 # dry run Google formatter
 echo "download path : $DOWNLOAD_PATH"/google-java-format-1.10.0-all-deps.jar
@@ -29,6 +63,8 @@ files_to_be_changed=$( java -jar "$DOWNLOAD_PATH"/google-java-format-1.10.0-all-
 
 # if there are any stage changes
 if  [[ -n "$files_to_be_changed" ]]; then
+
+
     echo "Formatter Results:"
     echo "Analyzing java file(s) using google-java-format-1.10.0-all-deps.jar..."
     echo "Incorrect formatting found:"
