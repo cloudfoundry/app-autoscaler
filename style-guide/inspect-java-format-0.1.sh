50ecdda0480030a649f01460b805f3b919699616
Arsalan Khan <muhammad.arsalan.khan@sap.com>
2021-08-03 15:47:20 +0200

    aas82 add java formatter git-hook

--- style-guide/inspect-java-format-0.1.sh
+++ style-guide/inspect-java-format-0.1.sh
@@ -10,6 +10,8 @@ CHECKSTYLE_CONFIG="google_checks.xml"
 CHECKSTYLE_CONFIG_PATH="$REPO_PATH"/style-guide/"$CHECKSTYLE_CONFIG"
 DOWNLOAD_PATH="$REPO_PATH"/.cache
 CHECKSTYLE_JAR_NAME="checkstyle-8.44-all.jar"
+GOOGLE_JAR_NAME="google-java-format-1.11.0-all-deps.jar"
+GOOGLE_JAR_VERSION="1.11.0"
 JAVA_SOURCES_PATH="$REPO_PATH"/scheduler/src/
 echo "Current Configs..."
 echo "REPO_PATH: $REPO_PATH"
@@ -21,59 +23,61 @@ echo "CHECKSTYLE_CONFIG_PATH: $CHECKSTYLE_CONFIG_PATH"
 
 function download_google_formatter {
     cd "$DOWNLOAD_PATH"
-    if [ ! -f "$DOWNLOAD_PATH"/google-java-format-1.10.0-all-deps.jar ] ; then
-        curl -LJO "https://github.com/google/google-java-format/releases/download/v1.10.0/google-java-format-1.10.0-all-deps.jar"
-        chmod 755 google-java-format-1.10.0-all-deps.jar
+    if [ ! -f "$DOWNLOAD_PATH"/"$GOOGLE_JAR_NAME" ] ; then
+        curl -LJO "https://github.com/google/google-java-format/releases/download/v$GOOGLE_JAR_VERSION/$GOOGLE_JAR_NAME"
+        chmod 755 "$GOOGLE_JAR_NAME"
     fi
 }
 
 function download_checkstyle {
     cd "$DOWNLOAD_PATH"
-    echo "I am download_checkstyle"
     if [ ! -f "$DOWNLOAD_PATH"/"$CHECKSTYLE_JAR_NAME" ] ; then
         curl -LJO "https://github.com/checkstyle/checkstyle/releases/download/checkstyle-8.44/checkstyle-8.44-all.jar"
         chmod 755 "$CHECKSTYLE_JAR_NAME"
     fi
 }
 
-
-
+# grep only stage java files
+changed_java_files=$(git diff --cached --name-only --diff-filter=ACMR | grep ".*java$" )
 if  [[ -n "$changed_java_files" ]]; then
     mkdir -p "$DOWNLOAD_PATH"
     download_google_formatter
     download_checkstyle
     cd "$REPO_PATH"
-
 fi
+# replace newlines with space
+changed_java_files=$(echo "$changed_java_files" |tr '\n' ' ')
 
-download_checkstyle
-changed_java_files=$(git diff --cached --name-only --diff-filter=ACMR | grep ".*java$" )
-echo changed_java_files
-echo "...."
+#####################################################################
+#   Checkstyle                                                      #
+#####################################################################
+#echo "executing java -jar "$DOWNLOAD_PATH"/"$CHECKSTYLE_JAR_NAME" -c "$CHECKSTYLE_CONFIG_PATH" $changed_java_files"
+java \
+  --add-exports jdk.compiler/com.sun.tools.javac.api=ALL-UNNAMED \
+  --add-exports jdk.compiler/com.sun.tools.javac.file=ALL-UNNAMED \
+  --add-exports jdk.compiler/com.sun.tools.javac.parser=ALL-UNNAMED \
+  --add-exports jdk.compiler/com.sun.tools.javac.tree=ALL-UNNAMED \
+  --add-exports jdk.compiler/com.sun.tools.javac.util=ALL-UNNAMED \
+  -jar "$DOWNLOAD_PATH"/"$CHECKSTYLE_JAR_NAME" -c "$CHECKSTYLE_CONFIG_PATH" $changed_java_files
 
-    #checkstyle
-    echo "executing java -jar "$DOWNLOAD_PATH"/"$CHECKSTYLE_JAR_NAME" -c "$CHECKSTYLE_CONFIG_PATH" $changed_java_files"
-    checkstyle_result=$(java -jar "$DOWNLOAD_PATH"/"$CHECKSTYLE_JAR_NAME" -c "$CHECKSTYLE_CONFIG_PATH" $JAVA_SOURCES_PATH )
-    echo "exiting"
-    echo "results: $checkstyle_result"
 
-# dry run Google formatter
-echo "download path : $DOWNLOAD_PATH"/google-java-format-1.10.0-all-deps.jar
-files_to_be_changed=$( java -jar "$DOWNLOAD_PATH"/google-java-format-1.10.0-all-deps.jar -n $changed_java_files)
-
+#####################################################################
+#   Google Formatter                                                #
+#####################################################################
+echo "============================================================"
+echo "Google Formatting using $DOWNLOAD_PATH"/"$GOOGLE_JAR_NAME..."
+files_to_be_formatted=$( java -jar "$DOWNLOAD_PATH"/"$GOOGLE_JAR_NAME" -n $changed_java_files)
 # if there are any stage changes
-if  [[ -n "$files_to_be_changed" ]]; then
-
-
+if  [[ -n "$files_to_be_formatted" ]]; then
     echo "Formatter Results:"
-    echo "Analyzing java file(s) using google-java-format-1.10.0-all-deps.jar..."
+    echo "Analyzing java file(s) using "$GOOGLE_JAR_NAME"..."
     echo "Incorrect formatting found:"
-
-
-    files_to_be_changed=$(echo $files_to_be_changed|tr -d '\n')
-    echo "$files_to_be_changed"
+    files_to_be_formatted=$(echo "$files_to_be_formatted" |tr '\n' ' ')
+    echo "$files_to_be_formatted"
     echo "Please correct the formatting of the files(s) using one of the following options:"
-    echo "  - java -jar "$DOWNLOAD_PATH"/google-java-format-1.10.0-all-deps.jar -replace ${files_to_be_changed[*]}"
-    echo "  - Reformat Code - IntelliJ or Format Document - Eclipse (google code style required)"
+    echo "   java -jar "$DOWNLOAD_PATH"/"$GOOGLE_JAR_NAME" -replace $files_to_be_formatted"
+    echo "   Reformat Code - IntelliJ or Format Document - Eclipse (google code style required)"
     exit 2
 fi
+
+
