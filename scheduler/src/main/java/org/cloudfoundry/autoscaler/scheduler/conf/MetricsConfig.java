7e630e24ea7c828f44cc98ac20ed10985b31dbc8
Kevin Cross <kevin.cross@sap.com>
2021-08-19 09:30:19 +0100

    Improvement: minor changes to the makefile to have better output and a quicker buildtools target.

--- scheduler/src/main/java/org/cloudfoundry/autoscaler/scheduler/conf/MetricsConfig.java
+++ scheduler/src/main/java/org/cloudfoundry/autoscaler/scheduler/conf/MetricsConfig.java
@@ -1,9 +1,51 @@
 package org.cloudfoundry.autoscaler.scheduler.conf;
 
+import com.sun.net.httpserver.Authenticator;
+import com.sun.net.httpserver.BasicAuthenticator;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpPrincipal;
 import io.prometheus.client.exporter.HTTPServer;
 import java.io.IOException;
+import java.util.Map;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+class ApiRestBasicAuthentication extends BasicAuthenticator {
+  private final Map<String, String> users;
+
+  public ApiRestBasicAuthentication(String realm, Map<String,String> users) {
+    super(realm);
+    this.users=users;
+  }
+
+  @Override
+  public Authenticator.Result authenticate(HttpExchange exch) {
+    Authenticator.Result result=super.authenticate(exch);
+    if(result instanceof Authenticator.Success) {
+      HttpPrincipal principal=((Authenticator.Success)result).getPrincipal();
+      String requestMethod=exch.getRequestMethod();
+
+      if( ) {
+        return new return new Authenticator.Failure(401);
+      }
+      return result;
+
+    }
+  }
+
+  @Override
+  public boolean checkCredentials(String user, String pwd) {
+    String passHash = users.get(user);
+     if (passHash != null && passHash.equals(pwd){
+      return gs;
+    }
+    return java.net.Authenticator.
+    int authCode = authentication.authenticate(user, pwd);
+    return authCode ==
+  }
+
+}
+
 
 @Configuration
 public class MetricsConfig {
