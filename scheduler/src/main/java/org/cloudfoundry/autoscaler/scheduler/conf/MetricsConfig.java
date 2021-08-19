99506dbba05e078fc368a8d07fdd6d3d7b3c831d
Kevin Cross <kevin.cross@sap.com>
2021-08-19 12:25:32 +0100

    Fixes for the properties files.

--- scheduler/src/main/java/org/cloudfoundry/autoscaler/scheduler/conf/MetricsConfig.java
+++ scheduler/src/main/java/org/cloudfoundry/autoscaler/scheduler/conf/MetricsConfig.java
@@ -1,57 +1,38 @@
 package org.cloudfoundry.autoscaler.scheduler.conf;
 
-import com.sun.net.httpserver.Authenticator;
-import com.sun.net.httpserver.BasicAuthenticator;
-import com.sun.net.httpserver.HttpExchange;
-import com.sun.net.httpserver.HttpPrincipal;
+import com.sun.net.httpserver.HttpContext;
+import com.sun.net.httpserver.HttpServer;
+import io.prometheus.client.CollectorRegistry;
 import io.prometheus.client.exporter.HTTPServer;
 import java.io.IOException;
-import java.util.Map;
+import java.net.InetSocketAddress;
+import java.util.stream.Stream;
+import org.cloudfoundry.autoscaler.scheduler.conf.MetricsConfiguration.AuthConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-class ApiRestBasicAuthentication extends BasicAuthenticator {
-  private final Map<String, String> users;
-
-  public ApiRestBasicAuthentication(String realm, Map<String,String> users) {
-    super(realm);
-    this.users=users;
-  }
-
-  @Override
-  public Authenticator.Result authenticate(HttpExchange exch) {
-    Authenticator.Result result=super.authenticate(exch);
-    if(result instanceof Authenticator.Success) {
-      HttpPrincipal principal=((Authenticator.Success)result).getPrincipal();
-      String requestMethod=exch.getRequestMethod();
-
-      if( ) {
-        return new return new Authenticator.Failure(401);
-      }
-      return result;
-
-    }
-  }
-
-  @Override
-  public boolean checkCredentials(String user, String pwd) {
-    String passHash = users.get(user);
-     if (passHash != null && passHash.equals(pwd){
-      return gs;
-    }
-    return java.net.Authenticator.
-    int authCode = authentication.authenticate(user, pwd);
-    return authCode ==
-  }
-
-}
-
 
 @Configuration
 public class MetricsConfig {
 
+  @Bean
+  HttpServer metricsServer(MetricsConfiguration config) throws IOException {
+    HttpServer server = HttpServer.create(new InetSocketAddress(config.getPort()), 3);
+    AuthConfig auth = config.getAuth();
+    Stream<HttpContext> contexts =
+        Stream.of(
+            server.createContext("/"),
+            server.createContext("/metrics"),
+            server.createContext("/-/healthy"));
+    if (config.isBasicAuthEnabled()) {
+      MetricsBasicAuthentication authenticator =
+          new MetricsBasicAuthentication(auth.getRealm(), auth.getUsers());
+      contexts.forEach(ctx -> ctx.setAuthenticator(authenticator));
+    }
+    return server;
+  }
+
   @Bean(destroyMethod = "stop")
-  public HTTPServer metricsServer(MetricsConfiguration config) throws IOException {
-    return new HTTPServer(config.getPort());
+  HTTPServer metricsServer(HttpServer server) throws IOException {
+    return new HTTPServer(server, CollectorRegistry.defaultRegistry, false);
   }
 }
--- scheduler/src/main/java/org/cloudfoundry/autoscaler/scheduler/conf/MetricsConfiguration.java
+++ scheduler/src/main/java/org/cloudfoundry/autoscaler/scheduler/conf/MetricsConfiguration.java
@@ -1,5 +1,6 @@
 package org.cloudfoundry.autoscaler.scheduler.conf;
 
+import java.util.Map;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
@@ -7,5 +8,13 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @Data
 public class MetricsConfiguration {
 
+  @Data
+  public static class AuthConfig {
+    private Map<String, String> users;
+    private String realm;
+  }
+
   private int port;
+  private boolean basicAuthEnabled = true;
+  private AuthConfig auth;
 }
