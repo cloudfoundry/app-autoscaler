36c5c52d23419748e2a1512d1febcc9d58a85c74
d070324 <aadesh.misra@sap.com>
2021-08-25 12:11:16 +0200

    revert changes to rename the property back to healthserver

--- scheduler/src/main/java/org/cloudfoundry/autoscaler/scheduler/conf/MetricsConfiguration.java
+++ scheduler/src/main/java/org/cloudfoundry/autoscaler/scheduler/conf/MetricsConfiguration.java
@@ -3,7 +3,7 @@ package org.cloudfoundry.autoscaler.scheduler.conf;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-@ConfigurationProperties(prefix = "scheduler.metrics")
+@ConfigurationProperties(prefix = "scheduler.healthserver")
 @Data
 public class MetricsConfiguration {
 
