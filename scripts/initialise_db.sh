c28550fff1c05b732055b2fa7d79a4e95db3cecb
Kevin Cross <kevin.cross@sap.com>
2021-08-26 12:05:44 +0100

    made the shell script work for mysql and postgresql

--- scripts/initialise_db.sh
+++ scripts/initialise_db.sh
@@ -4,7 +4,38 @@ set -e
 LOG_FILE=liqubase.log
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 ROOT="$SCRIPT_DIR/.."
-DB_USER=${DB_USER:-"root"}
+
+function usage {
+  echo -e "Usage: $0 [postgres|mysql]" >&2
+  echo "  Error: Please provide either postgres or mysql as a parameter" >&2
+  exit 1
+}
+
+if [ $# -ne 1 ]
+then
+    usage
+fi
+
+case $1 in
+mysql)
+  DB_USER=${DB_USER:-"root"}
+  URL="jdbc:mysql://127.0.0.1/autoscaler"
+  DRIVER="com.mysql.cj.jdbc.Driver"
+  PASSWORD_OPT=""
+  ;;
+postgres)
+  DB_USER=${DB_USER:-"postgres"}
+  DB_PASSWORD=${DB_PASSWORD:-"postgres"}
+  URL="jdbc:postgresql://127.0.0.1/autoscaler"
+  DRIVER="org.postgresql.Driver"
+  PASSWORD_OPT="--password=${DB_PASSWORD}"
+  ;;
+*)
+  usage
+  ;;
+esac
+
+
 
 files="api.db.changelog.yml \
        servicebroker.db.changelog.json \
@@ -35,10 +66,12 @@ error() {
 echo "# Applying liquibase change sets to: 'mysql://127.0.0.1/autoscaler'"
 for file in $files; do
   echo "  - applying: '$file'" | tee -a "${LOG_FILE}"
+  # shellcheck disable=SC2086
   java -cp "$class_path" liquibase.integration.commandline.Main \
-    --url jdbc:mysql://127.0.0.1/autoscaler\
-    --driver=com.mysql.cj.jdbc.Driver\
+    --url "${URL}"\
+    --driver="${DRIVER}"\
     --changeLogFile="$file"\
     --username="${DB_USER}"\
+    ${PASSWORD_OPT}\
      update >> "${LOG_FILE}"
 done
