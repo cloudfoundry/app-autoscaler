774334fb6799b435f6837675067cd3eb89eeb48c
Kevin Cross <kevin.cross@sap.com>
2021-08-26 11:37:31 +0100

    final touches to msql init script

--- scripts/initialise_db.sh
+++ scripts/initialise_db.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 set -e
+LOG_FILE=liqubase.log
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-ROOT=$(readlink -f "$SCRIPT_DIR/..")
+ROOT="$SCRIPT_DIR/.."
 DB_USER=${DB_USER:-"root"}
 
 files="api.db.changelog.yml \
@@ -14,7 +15,7 @@ files="api.db.changelog.yml \
        scalingengine.db.changelog.yml \
        operator.db.changelog.yml"
 
-class_path=$ROOT/src/autoscaler/api/db/:\
+class_path="$ROOT/src/autoscaler/api/db/:\
 $ROOT/db/target/lib/*:\
 $ROOT/src/autoscaler/servicebroker/db/:\
 $ROOT/scheduler/db/:\
@@ -22,12 +23,22 @@ $ROOT/scheduler/db/:\
 $ROOT/src/autoscaler/metricsserver/db/:\
 $ROOT/src/autoscaler/eventgenerator/db/:\
 $ROOT/src/autoscaler/scalingengine/db/:\
-$ROOT/src/autoscaler/operator/db/\
+$ROOT/src/autoscaler/operator/db/"
 
+[ -e "${LOG_FILE}" ] && rm "${LOG_FILE}"
+trap 'error' ERR
+trap "rm ${LOG_FILE} || echo \"no log file\"" EXIT
 
-
-
-
+error() {
+  [ -e "${LOG_FILE}" ] && cat "${LOG_FILE}"
+}
+echo "# Applying liquibase change sets to: 'mysql://127.0.0.1/autoscaler'"
 for file in $files; do
-java -cp "$class_path" liquibase.integration.commandline.Main --url jdbc:mysql://127.0.0.1/autoscaler --driver=com.mysql.cj.jdbc.Driver --changeLogFile="$file" --username="${DB_USER}" update
+  echo "  - applying: '$file'" | tee -a "${LOG_FILE}"
+  java -cp "$class_path" liquibase.integration.commandline.Main \
+    --url jdbc:mysql://127.0.0.1/autoscaler\
+    --driver=com.mysql.cj.jdbc.Driver\
+    --changeLogFile="$file"\
+    --username="${DB_USER}"\
+     update >> "${LOG_FILE}"
 done
