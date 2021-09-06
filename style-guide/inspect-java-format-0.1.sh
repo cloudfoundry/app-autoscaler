a2c209d20a4696b0a463a28eec659e286664af8f
Kevin Cross <kevin.cross@sap.com>
2021-09-06 11:36:06 +0100

    improved the pre-commit java format shell scripts

--- style-guide/inspect-java-format-0.1.sh
+++ style-guide/inspect-java-format-0.1.sh
@@ -10,7 +10,8 @@ DOWNLOAD_PATH="$REPO_PATH"/.cache
 CHECKSTYLE_JAR_NAME="checkstyle-8.44-all.jar"
 GOOGLE_JAR_VERSION="1.11.0"
 GOOGLE_JAR_NAME="google-java-format-${GOOGLE_JAR_VERSION}-all-deps.jar"
-
+script_dir="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+result_log="${script_dir}/result.txt"
 CHECKSTYLE_CONFIG_FILE_NAME="google_checks.xml"
 CHECKSTYLE_CONFIG_PATH="$REPO_PATH"/style-guide
 
@@ -31,30 +32,34 @@ function download_checkstyle {
 }
 
 # grep only stage java files
-changed_java_files=$(git diff --cached --name-only --diff-filter=ACMR | grep ".*java$" )
+changed_java_files=$(git diff --cached --name-only --diff-filter=ACMR \
+      | grep ".*java$" \
+      | tr '\n' ' ')
+
 if  [[ -n "$changed_java_files" ]]; then
     mkdir -p "$DOWNLOAD_PATH"
     download_google_formatter
     download_checkstyle
     cd "$REPO_PATH"
 fi
-# replace newlines with space
-changed_java_files=$(echo "$changed_java_files" |tr '\n' ' ')
 
 #####################################################################
 #   Checkstyle                                                      #
 #####################################################################
 echo "Running Checkstyle using $DOWNLOAD_PATH"/"$CHECKSTYLE_JAR_NAME..."
-
+trap "rm ${result_log} || echo " EXIT
 CHECKSTYLE_OUTPUT=$(java \
   --add-exports jdk.compiler/com.sun.tools.javac.api=ALL-UNNAMED \
   --add-exports jdk.compiler/com.sun.tools.javac.file=ALL-UNNAMED \
   --add-exports jdk.compiler/com.sun.tools.javac.parser=ALL-UNNAMED \
   --add-exports jdk.compiler/com.sun.tools.javac.tree=ALL-UNNAMED \
   --add-exports jdk.compiler/com.sun.tools.javac.util=ALL-UNNAMED \
-  -jar "$DOWNLOAD_PATH"/"$CHECKSTYLE_JAR_NAME" -p "${CHECKSTYLE_CONFIG_PATH}"/checkstyle.properties -c "${CHECKSTYLE_CONFIG_PATH}"/"${CHECKSTYLE_CONFIG_FILE_NAME}" -o=style-guide/result.txt $changed_java_files  )
-FILES_NEEDS_CORRECTION=$(echo "$CHECKSTYLE_OUTPUT" | sed  '0d;1d;$d' style-guide/result.txt)
-rm style-guide/result.txt
+  -jar "$DOWNLOAD_PATH"/"$CHECKSTYLE_JAR_NAME" \
+  -p "${CHECKSTYLE_CONFIG_PATH}"/checkstyle.properties \
+  -c "${CHECKSTYLE_CONFIG_PATH}"/"${CHECKSTYLE_CONFIG_FILE_NAME}" \
+  -o "${result_log}" $changed_java_files  )
+FILES_NEEDS_CORRECTION=$(echo "$CHECKSTYLE_OUTPUT" | sed  '0d;1d;$d' "$result_log")
+
 
 if  [[ -n "$FILES_NEEDS_CORRECTION" ]]; then
   echo "$FILES_NEEDS_CORRECTION"
@@ -73,9 +78,11 @@ files_to_be_formatted=$( java \
                     --add-exports jdk.compiler/com.sun.tools.javac.tree=ALL-UNNAMED \
                     --add-exports jdk.compiler/com.sun.tools.javac.util=ALL-UNNAMED \
                     -jar "$DOWNLOAD_PATH"/"$GOOGLE_JAR_NAME"  -n --skip-javadoc-formatting $changed_java_files)
+
 # if there are any stage changes
 if  [[ -n "$files_to_be_formatted" ]]; then
     echo "Incorrect formatting found:"
+    echo -e "These file(s) have formatting issues: \n$files_to_be_formatted\n"
     files_to_be_formatted=$(echo "$files_to_be_formatted" |tr '\n' ' ')
     echo "Please correct the formatting of the files(s) using one of the following options:"
     echo "   java --add-exports jdk.compiler/com.sun.tools.javac.api=ALL-UNNAMED --add-exports jdk.compiler/com.sun.tools.javac.file=ALL-UNNAMED --add-exports jdk.compiler/com.sun.tools.javac.parser=ALL-UNNAMED --add-exports jdk.compiler/com.sun.tools.javac.tree=ALL-UNNAMED --add-exports jdk.compiler/com.sun.tools.javac.util=ALL-UNNAMED  -jar "$DOWNLOAD_PATH"/"$GOOGLE_JAR_NAME" -replace --skip-javadoc-formatting $files_to_be_formatted"
