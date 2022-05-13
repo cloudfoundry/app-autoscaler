aaec9c1df47a3d563ebfb22b9835789991103e8b
Alan Moran <alan.moran@sap.com>
2022-05-13 16:34:17 +0200

    Remove unused comments

--- eventgenerator/aggregator/log_cache_client_test.go
+++ eventgenerator/aggregator/log_cache_client_test.go
@@ -4,34 +4,4 @@ import (
 	. "github.com/onsi/ginkgo/v2"
 )
 
-var _ = Describe("MetricClient", func() {
-	var ()
-
-	BeforeEach(func() {
-		//config = Config{}
-	})
-
-	JustBeforeEach(func() {
-	})
-
-	//Describe("When LogCache is enabled", func() {
-	//	var logCacheResponse LogCacheResponse
-
-	//	BeforeEach(func() {
-	//		logCacheResponse = LogCacheResponse{}
-	//		config.UseLogCache = true
-	//		config.MetricCollector.MetricCollectorURL = "https://log-cache.some-sys-domain.com"
-	//	})
-
-	//	JustBeforeEach(func() {
-	//		metricsServer.RouteToHandler("GET", config.MetricCollector.MetricCollectorURL,
-	//			ghttp.RespondWithJSONEncoded(http.StatusOK, &logCacheResponse))
-	//	})
-
-	//	It("creates request with log cache url", func() {
-	//		Expect(mc.GetAddrs()).To(BeEquivalentTo("https://log-cache.some-sys-domain.com"))
-	//		Expect(mc.EnableLogCache()).To(BeTrue())
-	//	})
-
-	//})
-})
+var _ = Describe("MetricClient", func() {})
