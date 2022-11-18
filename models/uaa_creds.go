8900e9e252e3c14237a2bd6d046079e5b09f8349
Alan Moran <alan.moran@sap.com>
2022-11-18 17:15:30 +0100

    Adds spec changes to event generator
    Signed-off-by: Arsalan Khan <muhammad.arsalan.khan@sap.com>

--- models/uaa_creds.go
+++ models/uaa_creds.go
@@ -1,7 +1,7 @@
 package models
 
 type UAACreds struct {
+	URL          string `yaml:"url" json:"url"`
 	ClientID     string `yaml:"client_id" json:"clientID"`
 	ClientSecret string `yaml:"client_secret" json:"clientSecret"`
-	URL          string `yaml:"url" json:"url"`
 }
