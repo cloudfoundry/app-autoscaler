2b5d7be8d45d5049e8d9ec58b69886fc66b49d01
Alan Moran <bonzofenix@gmail.com>
2024-06-06 16:29:50 +0200

    Refactor server code

--- metricsforwarder/server/server.go
+++ metricsforwarder/server/server.go
@@ -2,7 +2,6 @@ package server
 
 import (
 	"fmt"
-	"os"
 	"time"
 
 	"code.cloudfoundry.org/app-autoscaler/src/autoscaler/cred_helper"
@@ -25,18 +24,37 @@ import (
 )
 
 func NewServer(logger lager.Logger, conf *config.Config, policyDb db.PolicyDB, credentials cred_helper.Credentials, allowedMetricCache cache.Cache, httpStatusCollector healthendpoint.HTTPStatusCollector, rateLimiter ratelimiter.Limiter) (ifrit.Runner, error) {
+	metricForwarder, err := createMetricForwarder(logger, conf)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create metric forwarder: %w", err)
+	}
+
+	mh := NewCustomMetricsHandler(logger, *metricForwarder, policyDb, allowedMetricCache)
+	authenticator, err := auth.New(logger, credentials)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create auth middleware: %w", err)
+	}
+
+	mfRouter := setupMetricForwarderRouter(logger, mh, authenticator, httpStatusCollector, rateLimiter)
+	healthRouter, err := createHealthRouter(policyDb, credentials, logger, conf, httpStatusCollector)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create health router: %w", err)
+	}
+
+	mainRouter := setupMainRouter(mfRouter, healthRouter)
+	return helpers.NewHTTPServer(logger, conf.Server, mainRouter)
+}
+
+func createMetricForwarder(logger lager.Logger, conf *config.Config) (*forwarder.MetricForwarder, error) {
 	metricForwarder, err := forwarder.NewMetricForwarder(logger, conf)
 	if err != nil {
 		logger.Error("failed-to-create-metricforwarder-server", err)
-		os.Exit(1)
+		return nil, err
 	}
+	return &metricForwarder, nil
+}
 
-	mh := NewCustomMetricsHandler(logger, metricForwarder, policyDb, allowedMetricCache)
-	authenticator, err := auth.New(logger, credentials)
-	if err != nil {
-		return nil, fmt.Errorf("failed to add auth middleware: %w", err)
-	}
-
+func setupMetricForwarderRouter(logger lager.Logger, mh *CustomMetricsHandler, authenticator *auth.Auth, httpStatusCollector healthendpoint.HTTPStatusCollector, rateLimiter ratelimiter.Limiter) *mux.Router {
 	httpStatusCollectMiddleware := healthendpoint.NewHTTPStatusCollectMiddleware(httpStatusCollector)
 	rateLimiterMiddleware := ratelimiter.NewRateLimiterMiddleware("appid", rateLimiter, logger.Session("metricforwarder-ratelimiter-middleware"))
 
@@ -47,14 +65,7 @@ func NewServer(logger lager.Logger, conf *config.Config, policyDb db.PolicyDB, c
 	mfRouter.Use(authenticator.Authenticate)
 	mfRouter.Get(routes.PostCustomMetricsRouteName).Handler(common.VarsFunc(mh.VerifyCredentialsAndPublishMetrics))
 
-	healthRouter, _ := createHealthRouter(policyDb, credentials, logger, conf, httpStatusCollector)
-
-	mainRouter := mux.NewRouter()
-	mainRouter.PathPrefix("/v1").Handler(mfRouter)
-	mainRouter.PathPrefix("/health").Handler(healthRouter)
-	mainRouter.PathPrefix("/").Handler(healthRouter)
-
-	return helpers.NewHTTPServer(logger, conf.Server, mainRouter)
+	return mfRouter
 }
 
 func createHealthRouter(policyDb db.PolicyDB, credDb cred_helper.Credentials, logger lager.Logger, conf *config.Config, httpStatusCollector healthendpoint.HTTPStatusCollector) (*mux.Router, error) {
@@ -66,7 +77,7 @@ func createHealthRouter(policyDb db.PolicyDB, credDb cred_helper.Credentials, lo
 	gatherer := createPrometheusRegistry(policyDb, httpStatusCollector, logger)
 	healthRouter, err := healthendpoint.NewHealthRouter(conf.Health, checkers, logger.Session("health-server"), gatherer, time.Now)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to create health router: %w", err)
 	}
 	return healthRouter, nil
 }
@@ -78,4 +89,12 @@ func createPrometheusRegistry(policyDb db.PolicyDB, httpStatusCollector healthen
 		httpStatusCollector,
 	}, true, logger.Session("metricsforwarder-prometheus"))
 	return promRegistry
+}
+
+func setupMainRouter(mfRouter, healthRouter *mux.Router) *mux.Router {
+	mainRouter := mux.NewRouter()
+	mainRouter.PathPrefix("/v1").Handler(mfRouter)
+	mainRouter.PathPrefix("/health").Handler(healthRouter)
+	mainRouter.PathPrefix("/").Handler(healthRouter)
+	return mainRouter
 }
