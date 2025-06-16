2300ba22fed0eda0752af29c80b677d12fcbc7c0
Jörg Weisbarth <joerg.weisbarth@sap.com>
2025-06-16 16:39:54 +0200

    Adds proper type for GUIDs in Cloud Foundry;

--- models/app.go
+++ models/app.go
@@ -1,6 +1,13 @@
 package models
 
-import "time"
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"os"
+	"regexp"
+	"time"
+)
 
 type ScalingType int
 type ScalingStatus int
@@ -44,4 +51,65 @@ type AppScalingResult struct {
 	Status            ScalingStatus `json:"status"`
 	Adjustment        int           `json:"adjustment"`
 	CooldownExpiredAt int64         `json:"cool_down_expired_at"`
+}
+
+
+
+// ================================================================================
+// GUIDs
+// ================================================================================
+
+// Globally unique identifier in the context of a “Cloud Foundry” installation;
+type CfGuid string
+
+func (g CfGuid) String() string {
+	return string(g)
+}
+
+type CfGuidParser struct {
+	regexp *regexp.Regexp
+}
+
+func NewCfGuidParser() CfGuidParser {
+	filePath := "../../../schema/json/shared_definitions.json"
+	jsonData, err := os.ReadFile(filePath)
+	if err != nil {
+		// fmt.Errorf("Could not read file \"%s\"\nError: %w", filePath, err)
+		panic(`Assumed guid-schema not found.
+This is a programming-error as the file must be on the hardcorded location.`)
+	}
+
+	type Schema struct {
+		Schemas struct {
+			Guid struct {
+				Pattern string `json:"pattern"`
+			} `json:"guid"`
+		} `json:"schemas"`
+	}
+	var schema Schema
+	json.Unmarshal(jsonData, schema)
+	pattern := schema.Schemas.Guid.Pattern
+
+	r, err := regexp.CompilePOSIX(pattern)
+	if err != nil {
+		panic(`The provided pattern is invalid.
+This is a programming-error as the pattern must be a valid POSIX-regexp.`)
+	}
+
+	return CfGuidParser{regexp: r}
+}
+
+func (p CfGuidParser) Parse(rawGuid string) (CfGuid, error) {
+	matched := p.regexp.MatchString(rawGuid) // regexp.MatchString(p.regexp, rawGuid)
+	if ! matched {
+		msg := fmt.Sprintf("The provided string does not look like a Cloud Foundry GUID: %s", rawGuid)
+		return "<guid-parsing-error>", errors.New(msg)
+	}
+
+	return CfGuid(rawGuid), nil
+}
+
+func ParseGuid(rawGuid string) (CfGuid, error) {
+	p := NewCfGuidParser()
+	return p.Parse(rawGuid)
 }
