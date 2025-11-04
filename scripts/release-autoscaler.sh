9c40a2c0cd9f93d53b19558656cd893dda54c7ea
Alan Moran <bonzofenix@gmail.com>
2025-11-04 12:51:58 +0100

    Remove debug mode and enforce strict error handling in release-autoscaler.sh

--- scripts/release-autoscaler.sh
+++ scripts/release-autoscaler.sh
@@ -1,7 +1,6 @@
 #! /usr/bin/env bash
 # shellcheck disable=SC2154,SC1091
 
-set -x
 set -euo pipefail
 script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 source "${script_dir}/vars.source.sh"
