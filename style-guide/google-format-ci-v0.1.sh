055e8fb9a24b490652271b48af4336a8b7a92d84
Alan Moran <bonzofenix@gmail.com>
2025-11-27 15:04:28 +0100

    Update google-format-ci-v0.1.sh to use double brackets for conditionals

--- style-guide/google-format-ci-v0.1.sh
+++ style-guide/google-format-ci-v0.1.sh
@@ -6,13 +6,13 @@ export PATH=${HOME}/go/bin:${PATH}
 ###################################################################################################
 GOOGLE_JAR_VERSION=${GOOGLE_JAR_VERSION:-"1.22.0"}
 GOOGLE_JAR_NAME=${GOOGLE_JAR_NAME:-"google-java-format-${GOOGLE_JAR_VERSION}-all-deps.jar"}
-! [ -e "$GOOGLE_JAR_NAME" ] && \
+! [[ -e "$GOOGLE_JAR_NAME" ]] && \
   curl -fLJO "https://github.com/google/google-java-format/releases/download/v$GOOGLE_JAR_VERSION/$GOOGLE_JAR_NAME"
 # shellcheck disable=SC2046
 files_to_be_formatted=$(java \
               -jar "${GOOGLE_JAR_NAME}" --dry-run --skip-javadoc-formatting $(find scheduler -name '*.java' ! -name 'CloudFoundryConfigurationProcessorTest.java'))
 
-if  [ -n "$files_to_be_formatted" ]; then
+if  [[ -n "$files_to_be_formatted" ]]; then
   echo "Formatter Results..."
   echo "Files require reformatting:"
   echo "${files_to_be_formatted}"
